3505d0d8314d9e200b4cd08fe5a1f9ed1b2414e8
rubinkim <kimyongha2011@gmail.com>
2023-12-28 18:38:54 +0900

    more on 2110.py

--- PARAMETRIC_SEARCH/2110_공유기설치.py
+++ PARAMETRIC_SEARCH/2110_공유기설치.py
@@ -61,4 +61,11 @@ at positions x1,...,xN (0 <= xi <= 1,000,000,000).
 His C (2 <= C <= N) cows don't like this barn layout and become aggressive towards each other once put into a stall. 
 To prevent the cows from hurting each other, FJ want to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. 
 What is the largest minimum distance?
+
+Input
+Line 1: Two space-separated integers: N and C
+Lines 2..N+1: Line i+1 contains an integer stall location, xi
+
+Output
+Line 1: One integer: the largest minimum distance
 """          
