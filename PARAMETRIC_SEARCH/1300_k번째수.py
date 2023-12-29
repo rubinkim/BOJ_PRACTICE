d63e658cd9285f5832527e8fb28ce215d15c6987
rubinkim <kimyongha2011@gmail.com>
2023-12-29 16:29:00 +0900

    1300_k번째수.py

--- PARAMETRIC_SEARCH/1300_k번째수.py
+++ PARAMETRIC_SEARCH/1300_k번째수.py
@@ -11,3 +11,21 @@
 3
 7       6
 """
+
+n = int(input())
+k = int(input())
+
+start, end = 1, n * n
+
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 0
+    
+    for i in range(1, n+1):
+        cnt += min(mid//i, n)
+    
+    if cnt >= k:
+        end = mid - 1
+    else:
+        start = mid + 1
+print(start)
