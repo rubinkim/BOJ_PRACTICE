2453f5a1f2199413e18fa70ccd8183da006a379c
rubinkim <kimyongha2011@gmail.com>
2024-01-07 14:56:16 +0900

    2667_dfs

--- DFS_BFS/2667_단지번호붙이기.py
+++ DFS_BFS/2667_단지번호붙이기.py
@@ -1,8 +1,8 @@
 #-*- coding: utf-8 -*-
 
 """
- ?���? ?��?��	  메모�? ?��?��	?���?	   ?��?��	   맞힌 ?��?��	   ?��?�� 비율
-   1 �?	       128 MB	  173059	76869	  48816	         42.315%
+  시간 제한	   메모리 제한	  제출	   정답	    맞힌 사람	    정답 비율
+   1초	       128 MB	  173059	76869	  48816	         42.315%
   
 <그림 1>�? 같이 ?��?��각형 모양?�� �??���? ?��?��. 1??? 집이 ?��?�� 곳을, 0??? 집이 ?��?�� 곳을 ?��????��?��. 철수?�� ?�� �??���? �?�?�? ?��결된 집의 모임?�� ?���?�? ?��?��?���?, ?���??�� 번호�? 붙이?�� ?��?��. 
 ?��기서 ?��결되?��?��?�� 것�?? ?��?�� 집이 좌우, ?��??? ?��?��?���? ?���? 집이 ?��?�� 경우�? 말한?��. ???각선?��?�� 집이 ?��?�� 경우?�� ?��결된 것이 ?��?��?��. <그림 2>?�� <그림 1>?�� ?���?별로 번호�? 붙인 것이?��. 
@@ -94,7 +94,7 @@ print(cnt - 1)
 for x in list(num_dict.values()):
     print(x)
 """
-
+"""
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -134,4 +134,75 @@ for i in range(n):
 print(len(count_list))
 for i in range(len(count_list)):
     print(count_list[i])
+"""
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+    
+graph = [[0] * n for _ in range(n)]
+for i in range(n):
+    row = input()    
+    for j in range(len(row)):
+        if row[j] == '1':
+            graph[i][j] = 1
+path = []
+num_dict = {}
+
+def connected_component_mutant(graph, start_x, start_y, num):
+    global path, num_dict
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    q = deque()
+    
+    if start_x <= -1 or start_x >= n or start_y <= -1 or start_y >= n:
+        return False
+    if graph[start_x][start_y] == 0:
+        return False
+    if graph[start_x][start_y] == 1 and (start_x, start_y) in path:
+        return False
+    
+    if graph[start_x][start_y] == 1 and (start_x, start_y) not in path:
+        q.append((start_x, start_y))
+        path.append((start_x, start_y))
+        graph[start_x][start_y] = num
+        if num not in num_dict.keys():
+            num_dict[num] = 1
         
+        while q:
+            x, y = q.pop()
+            cnt = 0
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                
+                if nx <= -1 or nx >= n or ny <= -1 or ny >= n:
+                    continue
+                if graph[nx][ny] == 0:
+                    continue
+                if graph[nx][ny] == 1 and (nx, ny) in path:
+                    continue
+                if graph[nx][ny] == 1 and (nx, ny) not in path:
+                    q.append((nx, ny))
+                    path.append((nx, ny))
+                    graph[nx][ny] = num
+                    cnt += 1
+                    num_dict[num] += 1
+        
+        return True
+    return False
+
+cnt = 1
+for i in range(n):
+    for j in range(n):
+        if connected_component_mutant(graph, i, j, cnt) == True:
+            cnt += 1  
+
+print(cnt - 1)
+num_list = list(num_dict.values())
+num_list.sort()
+for x in num_list:
+    print(x)
