f511a45743cd09336516b3686fab04f3d5392c45
rubinkim <kimyongha2011@gmail.com>
2024-01-09 01:06:17 +0900

    1012_practice.py

--- DFS_BFS/1012_practice.py
+++ DFS_BFS/1012_practice.py
@@ -1,27 +1,6 @@
 #-*- coding: utf-8 -*-
 
 """
-2
-10 8 17
-0 0
-1 0
-1 1
-4 2
-4 3
-4 5
-2 4
-3 4
-7 4
-8 4
-9 4
-7 5
-8 5
-9 5
-7 6
-8 6
-9 6
-10 10 1
-5 5
 
 1
 5 3 6
@@ -68,54 +47,3 @@ for _ in range(int(input())):
     print(result)
 """   
 
-from collections import deque
-path = []
-num_dict = {}
-
-def connected_component_mutant(graph, start_y, start_x, num):
-    global path 
-    global num_dict
-    
-    dy = [0, 0, -1, 1]
-    dx = [-1, 1, 0, 0]    
-    
-    q = deque()
-    q.append((start_y, start_x))
-    path.append((start_y, start_x))
-    graph[start_y][start_x] = num
-    if num not in num_dict:
-        num_dict[num] = 1
-    else:
-        num_dict[num] += 1
-        
-        while q:
-            y, x = q.pop()                     # x, y = q.popleft()로 하면 bfs이다.
-            cnt = 0            
-            for i in range(4):                
-                ny = y + dy[i]
-                nx = x + dx[i]                
-                if ny <= -1 or ny >= n or nx <= -1 or nx >= m:
-                    continue
-                if graph[ny][nx] == 1 and (ny, nx) not in path:
-                    q.append((ny, nx))
-                    path.append((ny, nx))
-                    graph[ny][nx] = num
-                    cnt += 1
-                    num_dict[num] += 1        
-        return True
-    return False
-
-num_usecases = int(input())
-for i in range(num_usecases):
-    m, n, k = map(int, input().split())    
-    graph = [[0] * m for _ in range(n)]
-    for _ in range(k):
-        x, y = map(int, input().split())        
-        graph[y][x] = 1      
-        
-    ans = 1
-    for i in range(n):
-        for j in range(m):
-            if connected_component_mutant(graph, i, j, ans) == True:                
-                ans += 1
-    print(ans - 1)
