3c69c4e45d0e66e6c53054a5cd98cb83bba65a43
rubinkim <kimyongha2011@gmail.com>
2024-01-09 01:54:03 +0900

    1012_practice.py

--- DFS_BFS/1012_right.py
+++ DFS_BFS/1012_right.py
@@ -33,3 +33,80 @@
 4 0
 """
 
+# dfs 방식  : path 대신에 visited로 바꿨더니 동작한다.
+from collections import deque
+num_dict = {}
+
+def connected_component_mutant(graph, start_y, start_x, num):
+    global num_dict
+    dy = [0, 0, -1, 1]
+    dx = [-1, 1, 0, 0]    
+    
+    q = deque()
+    
+    if start_y <= -1 or start_y >= n or start_x <= -1 or start_x >= m:
+        return False
+    if graph[start_y][start_x] == 0:
+        return False
+    if graph[start_y][start_x] == 1 and visited[start_y][start_x]:
+        return False
+    
+    if graph[start_y][start_x] == 1 and not visited[start_y][start_x]:
+        q.append((start_y, start_x))
+        visited[start_y][start_x] = True
+        graph[start_y][start_x] = num
+        if num not in num_dict:
+            num_dict[num] = 1
+        else:
+            num_dict[num] += 1
+        
+        while q:
+            y, x = q.pop()                     # x, y = q.popleft()로 하면 bfs이다.
+            cnt = 0
+            
+            for i in range(4):                
+                ny = y + dy[i]
+                nx = x + dx[i]
+                
+                if ny <= -1 or ny >= n or nx <= -1 or nx >= m:
+                    continue
+                if graph[ny][nx] == 0:
+                    continue
+                if graph[ny][nx] == 1 and visited[ny][nx]:
+                    continue
+                if graph[ny][nx] == 1 and not visited[ny][nx]:
+                    q.append((ny, nx))
+                    visited[ny][nx] = True
+                    graph[ny][nx] = num
+                    cnt += 1
+                    num_dict[num] += 1
+        
+        return True
+    return False
+
+#input = sys.stdin.readline
+num_usecases = int(input())
+#print(f"number of usecases : {num_usecases}")
+#print()
+
+for i in range(num_usecases):
+    #print(f"usecase {i}")
+    m, n, k = map(int, input().split())
+    #print(f"m : {m},  n : {n},  k : {k}")
+    graph = [[0] * m for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]
+    for _ in range(k):
+        x, y = map(int, input().split())
+        #print(f"x : {x},  y : {y}")
+        graph[y][x] = 1
+        
+    #for row in graph:
+        #print(row)        
+        
+    ans = 1
+    for i in range(n):
+        for j in range(m):
+            if connected_component_mutant(graph, i, j, ans) == True:
+                ans += 1  
+
+    print(ans - 1)
