6e0fa866f81ce8d3ddecf986b5067f4d6d9b6e68
rubinkim <kimyongha2011@gmail.com>
2024-01-09 01:30:33 +0900

    1012_practice.py

--- DFS_BFS/1012_right.py
+++ DFS_BFS/1012_right.py
@@ -33,3 +33,53 @@
 4 0
 """
 
+from collections import deque
+
+def connected_component_mutant(graph, start_y, start_x, num):    
+    dy = [0, 0, -1, 1]
+    dx = [-1, 1, 0, 0]    
+    
+    q = deque()
+    
+    if start_y <= -1 or start_y >= n or start_x <= -1 or start_x >= m:
+        return False    
+    if graph[start_y][start_x] == 1 and not visited[start_y][start_x]:
+        q.append((start_y, start_x))
+        visited[start_y][start_x] = True
+        
+        while q:
+            y, x = q.pop()                     # x, y = q.popleft()로 하면 bfs이다.
+            cnt = 0
+            
+            for i in range(4):                
+                ny = y + dy[i]
+                nx = x + dx[i]
+                
+                if ny <= -1 or ny >= n or nx <= -1 or nx >= m:
+                    continue
+                if graph[ny][nx] == 1 and not visited[ny][nx]:
+                    q.append((ny, nx))
+                    visited[ny][nx] = True                    
+                    cnt += 1                           
+        return True
+    return False
+
+num_usecases = int(input())
+
+for i in range(num_usecases):    
+    m, n, k = map(int, input().split())    
+    graph = [[0] * m for _ in range(n)]
+    visited = [[False]*m for _ in range(n)]
+    for _ in range(k):
+        x, y = map(int, input().split())        
+        graph[y][x] = 1       
+        
+    ans = 1
+    for i in range(n):
+        for j in range(m):
+            if connected_component_mutant(graph, i, j, ans) == True:
+                ans += 1  
+
+    print(ans - 1)
+    
+    
