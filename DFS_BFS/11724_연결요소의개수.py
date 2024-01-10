a964453bf581ea084c069c9d9d1a71778d3448de
rubinkim <kimyongha2011@gmail.com>
2024-01-10 22:04:49 +0900

    11724

--- DFS_BFS/11724_연결요소의개수.py
+++ DFS_BFS/11724_연결요소의개수.py
@@ -17,7 +17,7 @@
 5 4
 2 4
 2 3
-""" 
+
 
 ### BFS using visited
 import sys
@@ -98,4 +98,50 @@ for i in range(1, n + 1):
             cnt += 1  # 연결요소 를 +1개 해준다.
 
 print(cnt)               
+"""
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(graph, start):
+    q = deque([start])
+    sub_path = []
+    while q:
+        now = q.popleft()
+        if now not in sub_path:
+            sub_path.append(now)
+        for nxt in graph[now]:
+            if nxt not in path:
+                sub_path.append(nxt)
+                q.append(nxt)
+    path.append(sub_path)
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
     
+for row in graph:
+    print(row)
+
+# 방문처리
+path = []
+cnt = 0  # 컴포넌트 그래프 개수 저장
+
+# 1~N번 노드를 각각돌면서
+for i in range(1, n + 1):
+    if i not in path:  # 만약 방문하지 않았다면
+        if not graph[i]:           # 만약 그래프가 비어있다면
+            cnt += 1               # 개수 한개 추가
+            path.append(i)         # 방문처리
+        else:
+            dfs(graph, i)  # 해당 i를 시작노드로 bfs를 돈다.
+            cnt += 1  # 연결요소 를 +1개 해준다.
+
+print()
+print(path)
+print(cnt)      
