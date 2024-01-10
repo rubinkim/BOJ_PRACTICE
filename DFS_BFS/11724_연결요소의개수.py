9b8c46fa9e82d857163556da86c74d256bd83e7b
rubinkim <kimyongha2011@gmail.com>
2024-01-11 07:00:48 +0900

    11724

--- DFS_BFS/11724_연결요소의개수.py
+++ DFS_BFS/11724_연결요소의개수.py
@@ -160,5 +160,17 @@ from collections import deque
 sys.setrecursionlimit(5000)
 
 input = sys.stdin.readline
+n, m = map(int, input().split())
+path = []
+cnt = 0
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-def 
+
+def dfs_recursion(graph, start, depth):
+    path.append(start)
+    
+    
