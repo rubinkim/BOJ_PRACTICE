1d951dd79f8d1d1078cab6099a4e0702ccb9ffa8
rubinkim <kimyongha2011@gmail.com>
2024-01-10 12:05:34 +0900

    more on 11724

--- DFS_BFS/11724_연결요소의개수.py
+++ DFS_BFS/11724_연결요소의개수.py
@@ -138,5 +138,41 @@ for i in range(1, n+1):
         
 print(result)
 
+### 정답
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(start):
+    queue = deque([start])
+    if not visited[start]:
+        visited[start] = True
+    while queue:
+        node = queue.popleft()
+        for i in graph[node]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 방문처리
+visited = [False] * (1 + N)
+count = 0  # 컴포넌트 그래프 개수 저장
+
+# 1~N번 노드를 각각돌면서
+for i in range(1, N + 1):
+    if not visited[i]:  # 만약 방문하지 않았다면
+        bfs(i)  # 해당 i를 시작노드로 bfs를 돈다.
+        count += 1  # 연결요소 를 +1개 해준다.
+
+print(count)
+
                  
     
