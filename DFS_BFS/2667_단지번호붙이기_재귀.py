c08bb35d3ce6bbba659de84ac41fc575cdd8a191
rubinkim <kimyongha2011@gmail.com>
2024-01-12 00:54:43 +0900

    2667_단지번호붙이기_재귀.py

--- DFS_BFS/2667_단지번호붙이기_재귀.py
+++ DFS_BFS/2667_단지번호붙이기_재귀.py
@@ -47,4 +47,10 @@ def dfs_recursion(graph, start_x, start_y, num, depth):
                 continue
             if graph[nx][ny] == 1 and (nx, ny) in path:
                 continue
-            
+            if graph[nx][ny] == 1 and (nx, ny) not in path:
+                dfs_recursion(graph, nx, ny, num, depth+1)
+                cnt += 1
+                num_dict[num] += 1
+                cell_dict[num].append((nx, ny))
+        return True
+    return False
