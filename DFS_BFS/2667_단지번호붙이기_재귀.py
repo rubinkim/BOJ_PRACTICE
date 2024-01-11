011eb873ee4796fc3c6a34c7ece0f95cfee30011
rubinkim <kimyongha2011@gmail.com>
2024-01-12 00:33:53 +0900

    2667_단지번호붙이기_재귀.py

--- DFS_BFS/2667_단지번호붙이기_재귀.py
+++ DFS_BFS/2667_단지번호붙이기_재귀.py
@@ -20,3 +20,20 @@ graph = []
 path = []
 num_dict, cell_dict = {}, {}
 
+def dfs_recursion(graph, start_x, start_y, num, depth):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    if start_x <= -1 or start_x >= n or start_y <= -1 or start_y >= n:
+        return False
+    if graph[start_x][start_y] == 0:
+        return False
+    if graph[start_x][start_y] == 1 and (start_x, start_y) in path:
+        return False
+    if graph[start_x][start_y] == 1 and (start_x, start_y) not in path:
+        path.append((start_x, start_y))
+        graph[start_x][start_y] = num
+        if num not in num_dict.keys():
+            num_dict[num] = 1
+        if num not in cell_dict.keys():
+            cell_dict[num] = [(start_x, start_y)]
