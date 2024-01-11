8dca770b1cc6fb01ac0f0958bdc15a6785812636
rubinkim <kimyongha2011@gmail.com>
2024-01-11 12:36:30 +0900

    2667

--- DFS_BFS/2667_단지번호붙이기.py
+++ DFS_BFS/2667_단지번호붙이기.py
@@ -139,21 +139,22 @@ for row in graph:
     print(row)    
   
 def dfs_deque(graph, start_x, start_y):
-    q = deque([(start_x, start_y)])
-    
-    while q:
-        x, y = q.pop()  
-        if (x, y) not in path:
-            path.append((x, y))    
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if nx <= -1 or nx >= n or ny <= -1 or ny >= n:
-                    continue
-                if graph[nx][ny] == 1 and (nx, ny) not in path:
-                    path.append((nx, ny))
-                    q.append((nx, ny))
-                    print(path)
+    if graph[start_x][start_y] == 1 and (start_x, start_y) not in path:
+        q = deque([(start_x, start_y)])
+        path.append((start_x, start_y))
+        while q:
+            x, y = q.popleft()  
+            if (x, y) not in path:
+                path.append((x, y))    
+                for i in range(4):
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+                    if nx <= -1 or nx >= n or ny <= -1 or ny >= n:
+                        continue
+                    if graph[nx][ny] == 1 and (nx, ny) not in path:
+                        path.append((nx, ny))
+                        q.append((nx, ny))
+ 
 
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 path = []
@@ -165,6 +166,7 @@ for i in range(n):
             if graph[i][j] == 1:
                 dfs_deque(graph, i, j)
                 cnt += 1
+                print(f"{i, j} : {path}")
 print(cnt)
                 
 
