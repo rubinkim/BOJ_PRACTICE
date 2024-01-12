9715726f7bfeca30804da0ecfebf48343b88d3cd
rubinkim <kimyongha2011@gmail.com>
2024-01-12 23:13:49 +0900

    10026_적록색약.py

--- DFS_BFS/10026_적록색약.py
+++ DFS_BFS/10026_적록색약.py
@@ -18,9 +18,10 @@ for i in range(n):
     graph.append(input().rstrip())
 
 path = []
-color_dict = {}
+cc_num_list = []
+cnt = 0
 
-def dfs(graph, start_x, start_y):
+def dfs(graph, start_x, start_y, num):
     global path, color_dict, cnt
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
@@ -28,25 +29,24 @@ def dfs(graph, start_x, start_y):
     if start_x <= -1 or start_x >= n or start_y <= -1 or start_y >= n:
         return False
     RGB = ["R", "G", "B"]
-    for color in RGB:
-        if graph[start_x][start_y] == color and (start_x, start_y) not in path:
-            q = deque([(start_x, start_y)])
-            path.append((start_x, start_y))
-            if color not in color_dict.keys():
-                color_dict[color] = 1
-            else:
-                color_dict[color] += 1        
-            while q:
-                x, y = q.pop()   
-                for i in range(4):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    if nx <= -1 or nx >= n or ny <= -1 or ny >= n:
-                        continue
-                    if graph[nx][ny] == color and (nx, ny) not in path:
-                        q.append((nx, ny))
-                        path.append((nx, ny))
-                        color_dict[color] += 1
+    if (start_x, start_y) not in path:        
+        for color in RGB:
+            if graph[start_x][start_y] == color:
+                q = deque([(start_x, start_y)])
+                path.append((start_x, start_y))
+                graph[start_x][start_y] = num
+                cnt += 1      
+                while q:
+                    x, y = q.pop()   
+                    for i in range(4):
+                        nx = x + dx[i]
+                        ny = y + dy[i]
+                        if nx <= -1 or nx >= n or ny <= -1 or ny >= n:
+                            continue
+                        if graph[nx][ny] == color and (nx, ny) not in path:
+                            q.append((nx, ny))
+                            path.append((nx, ny))
+                            color_dict[color] += 1
         return True
     return False
 
@@ -56,6 +56,7 @@ for i in range(n):
         if  dfs(graph, i, j):
             ans +=1
 print(ans)   
+print(color_dict)
         
     
     
