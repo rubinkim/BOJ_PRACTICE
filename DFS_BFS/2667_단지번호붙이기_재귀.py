da3a3f3f269297542b85c5f7b1eb84921c15429d
rubinkim <kimyongha2011@gmail.com>
2024-01-12 11:35:54 +0900

    more on dfs_recursion

--- DFS_BFS/2667_단지번호붙이기_재귀.py
+++ DFS_BFS/2667_단지번호붙이기_재귀.py
@@ -18,11 +18,12 @@ input = sys.stdin.readline
 n = int(input())
 graph = []
 path = []
+cnt = 0
 
-
-def dfs_recursion(graph, start_x, start_y, num, depth):
+def dfs_recursion(graph, start_x, start_y, num):
+    global cnt
     dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+    dy = [0, 0, -1, 1]    
     
     if start_x <= -1 or start_x >= n or start_y <= -1 or start_y >= n:
         return False
@@ -33,8 +34,8 @@ def dfs_recursion(graph, start_x, start_y, num, depth):
     if graph[start_x][start_y] == 1 and (start_x, start_y) not in path:
         path.append((start_x, start_y))
         graph[start_x][start_y] = num
-        
-        cnt = 0
+        cnt += 1
+
         for i in range(4):
             nx, ny = start_x + dx[i], start_y + dy[i]
             if nx <= -1 or nx >= n or ny <= -1 or ny >= n:
@@ -44,17 +45,19 @@ def dfs_recursion(graph, start_x, start_y, num, depth):
             if graph[nx][ny] == 1 and (nx, ny) in path:
                 continue
             if graph[nx][ny] == 1 and (nx, ny) not in path:
-                dfs_recursion(graph, nx, ny, num, depth+1)
-                cnt += 1
+                dfs_recursion(graph, nx, ny, num)
         return True
     return False
 
 ans = 0
 for i in range(n):
     for j in range(n):
-        if not dfs_recursion(graph, i, j, ans+1, 0):
-            continue
-        else:
+        if dfs_recursion(graph, i, j, ans+1, 0):
+            path.append(cnt)
             ans += 1
+            cnt = 0
 
+path.sort()
 print(ans)
+for i in range(len(path)):
+    print(path[i])
