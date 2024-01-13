2dd7670409776093f3e5181098145aae4385f29b
rubinkim <kimyongha2011@gmail.com>
2024-01-13 10:33:01 +0900

    2468_safetyzone.py

--- DFS_BFS/2468_안전지대.py
+++ DFS_BFS/2468_안전지대.py
@@ -18,3 +18,17 @@
 
 정답은 (5, 6)
 """
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split().rstrip())))
+    
+print(f"n : {n}")
+print()
+for row in graph:
+    print(row)
