02b6d3c0f17497e8dda3bee986840c91af1db838
rubinkim <kimyongha2011@gmail.com>
2024-01-16 14:39:16 +0900

    1987_alphabet.py

--- DFS_BFS/1987_알파벳.py
+++ DFS_BFS/1987_알파벳.py
@@ -24,20 +24,45 @@ HMCHH
 """
 
 from collections import deque
-import sys
-
-input = sys.stdin.readline 
 
 r, c = map(int, input().split())
 graph = [[0] * c for _ in range(r)]
-for i in range(r):
-    row = input()
-    print(f"row : {row}")
-    for j, x in enumerate(row):
+for i in range(r):    
+    for j, x in enumerate(input()):
         graph[i][j] = x
 
 for row in graph:
     print(row)
     
+visited = [[False] * c for _ in range(r)]
+path = []
+cnt = 1
 
+def logest_path(graph, start_x, start_y):
+    global visited, path, cnt
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+    
+    if start_x <= -1 or start_x >= r or start_y <= -1 or start_y >= c:
+        return False
+    if graph[start_x][start_y] not in path and not visited[start_x][start_y]:
+        q = deque()
+        q.append((start_x, start_y))
+        visited[start_x][start_y] = True
+        cnt += 1
+        
+        while q:
+            x, y = q.pop()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if graph[nx][ny] not in path and not visited[nx][ny]:
+                    q.append((nx, ny))
+                    visited[nx][ny] = True
+                    cnt += 1
+        return True
+    return False
+
+logest_path(graph, 0, 0)
+print(path)
+print(cnt)
           
