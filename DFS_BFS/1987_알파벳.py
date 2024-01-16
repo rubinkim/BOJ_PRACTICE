4392d21acf59672fab2c020ea42114b45ad45e4c
rubinkim <kimyongha2011@gmail.com>
2024-01-16 16:01:46 +0900

    1987_alphabet.py

--- DFS_BFS/1987_알파벳.py
+++ DFS_BFS/1987_알파벳.py
@@ -43,24 +43,20 @@ def logest_path(graph, start_x, start_y):
     if start_x <= -1 or start_x >= r or start_y <= -1 or start_y >= c:
         return False
     if graph[start_x][start_y] not in path and not visited[start_x][start_y]:
-        q = []
-        q.append((start_x, start_y))
+
         path.append(graph[start_x][start_y])
         visited[start_x][start_y] = True
         cnt += 1
 
-        while q:
-            x, y = q.pop()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if nx <= -1 or nx >= r or ny <= -1 or ny >= c:
-                    continue
-                if graph[nx][ny] not in path and not visited[nx][ny]:
-                    path.append(graph[nx][ny])
-                    q.append((nx, ny))
-                    visited[nx][ny] = True
-                    cnt += 1
+
+        for i in range(4):
+            nx = start_x + dx[i]
+            ny = start_y + dy[i]
+            if nx <= -1 or nx >= r or ny <= -1 or ny >= c:
+                continue
+            if graph[nx][ny] not in path and not visited[nx][ny]:
+                logest_path(graph, nx, ny)
+
 
         return True
     return False
