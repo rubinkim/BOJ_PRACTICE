5641bb4f3cad6b1455b242de5dcb97012625d0df
rubinkim <kimyongha2011@gmail.com>
2024-01-17 19:26:09 +0900

    1987_alphabet

--- DFS_BFS/1987_알파벳.py
+++ DFS_BFS/1987_알파벳.py
@@ -31,36 +31,48 @@ for i in range(r):
 for row in graph:
     print(row)
 print()
-    
-visited = [[False] * c for _ in range(r)]
-path = []
+
+total_path = []    
 cnt = 0
 
-def logest_path(graph, start_x, start_y, num):
-    global visited, path, cnt
+def logest_path(graph, start_x, start_y):
+    global cnt, total_path
     dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-    
+    path = []
+    visited = [[False] * c for _ in range(r)]
     if start_x <= -1 or start_x >= r or start_y <= -1 or start_y >= c:
         return False
-    if graph[start_x][start_y] not in path and not visited[start_x][start_y]:
-
+    if graph[start_x][start_y] not in path and not visited[start_x][start_y]:        
+        q = deque()
+        q.append((start_x, start_y))
         path.append(graph[start_x][start_y])
         visited[start_x][start_y] = True
         cnt += 1
-        
-        for i in range(4):
-            nx = start_x + dx[i]
-            ny = start_y + dy[i]
-            if nx <= -1 or nx >= r or ny <= -1 or ny >= c:
-                continue
-            if graph[nx][ny] not in path and not visited[nx][ny]:
-                logest_path(graph, nx, ny, num+1)
 
+        while q:
+            x, y = q.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if nx <= -1 or nx >= r or ny <= -1 or ny >= c:
+                    continue
+                if graph[nx][ny] not in path and not visited[nx][ny]:
+                    sub_dict = {}
+                    sub_path = []
+                    sub_dict[i] = graph[nx][ny]                    
+                    path.append(sub_dict[i])
+                    q.append((nx, ny))
+                    visited[nx][ny] = True
+                    cnt += 1
+                print(f"i : {i}, (x,y) : {(x,y)} sub_dict : {sub_dict}")
+                print()
+                total_path.append(path)
+                print(f"total_path : {total_path}")
 
         return True
     return False
 
-logest_path(graph, 0, 0, 0)
-print(path)
-print(cnt)
+logest_path(graph, 0, 0)
+print(f"path : {total_path}")
+print(f"cnt : {cnt}")
           
