48d10fe70a8975128db5f78b970306d2e1976b2c
rubinkim <kimyongha2011@gmail.com>
2024-01-23 13:28:40 +0900

    15650_N_and_M.py

--- BACKTRACKING/15650_N_and_M_2.py
+++ BACKTRACKING/15650_N_and_M_2.py
@@ -2,3 +2,17 @@
 """
 4 2
 """
+
+n, m = map(int, input().split())
+ans = []
+
+def back(start, end):
+    if len(ans) == m:
+        print(" ".join(map(str, ans)))
+    for i in range(start, end):
+        if i not in ans:
+            ans.append(i)
+            back(start+1, end)
+            ans.pop()
+            
+back(1, n+1)
