8b1d022e088438a0d3490c15e8c864f080ff38dc
rubinkim <kimyongha2011@gmail.com>
2024-02-08 18:18:41 +0900

    more on 14888

--- BACKTRACKING/14888_연산자_끼워넣기.py
+++ BACKTRACKING/14888_연산자_끼워넣기.py
@@ -81,11 +81,11 @@ def dfs(n, total, plus, minus, multiply, divide):
     if plus:
         dfs(n+1, total+num[n], plus-1, minus, multiply, divide)
     if minus:
-        dfs(n+1, total+num[n], plus, minus-1, multiply, divide)
+        dfs(n+1, total-num[n], plus, minus-1, multiply, divide)
     if multiply:
-        dfs(n+1, total+num[n], plus, minus, multiply-1, divide)
+        dfs(n+1, total*num[n], plus, minus, multiply-1, divide)
     if divide:
-        dfs(n+1, total+num[n], plus, minus, multiply, divide-1)
+        dfs(n+1, int(total//num[n]), plus, minus, multiply, divide-1)
         
 dfs(1, num[0], op[0], op[1], op[2], op[3])
 print(maximum)
