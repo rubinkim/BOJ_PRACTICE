9657c533ec29847b5f81b6dd20e9261218740d26
rubinkim <kimyongha2011@gmail.com>
2024-02-08 18:01:32 +0900

    more on 14888

--- BACKTRACKING/14888_연산자_끼워넣기.py
+++ BACKTRACKING/14888_연산자_끼워넣기.py
@@ -86,4 +86,6 @@ def dfs(n, total, plus, minus, multiply, divide):
     elif divide > 0:
         dfs(n+1, total+num[n], plus, minus, multiply, divide-1)
         
-    
+dfs(1, num[0], op[0], op[1], op[2], op[3])
+print(maximum)
+print(minimum)
