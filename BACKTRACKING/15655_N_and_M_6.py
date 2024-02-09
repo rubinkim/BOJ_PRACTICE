5402f6bb4fd794d4d4d4c8bbc3e062688a39c4e2
rubinkim <kimyongha2011@gmail.com>
2024-02-09 18:59:04 +0900

    15650.py

--- BACKTRACKING/15655_N_and_M_6.py
+++ BACKTRACKING/15655_N_and_M_6.py
@@ -5,27 +5,24 @@
 9 8 7 1
 """
 
-N, M = map(int, input().split())
-nums = list(map(int, input().split()))
-
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split()) 
+nums = list(map(int, input().split()))    
 ans = []
-v = [0] * (N+1)
 
 def dfs(n, lst):
+    global ans
+    v = [0] * (N+1)
     if n == M:
         ans.append(lst)
         return
     for j in range(1, N+1):
-        if j == 1:
-            if v[j] == 0:
-                v[j] = 1
-                dfs(n+1, lst+[nums[j]])
-                v[j] = 0
-        else:
-            if v[j] == 0 and all([nums[j] >= x for x in lst]):
-                v[j] = 1
-                dfs(n+1, lst+[nums[j]])
-                v[j] = 0
+        if v[j] == 0 and all([nums[j] >= x for x in lst]): 
+            #if (len(lst) == 0) or (len(lst) > 0 and all([nums[j] >= x for x in lst])):
+            v[j] = 1
+            dfs(n+1, lst + [nums[j]])
+            v[j] = 0
 
 dfs(0, [])
 for lst in ans:
