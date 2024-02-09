9096dc1481010ff61aa690edb663175295f5e68a
rubinkim <kimyongha2011@gmail.com>
2024-02-09 12:47:21 +0900

    15650

--- BACKTRACKING/15650_N_and_M_2.py
+++ BACKTRACKING/15650_N_and_M_2.py
@@ -2,7 +2,8 @@
 """
 4 2
 """
-
+"""
+# 나의 저급한 방법
 n, m = map(int, input().split())
 ans = []
 
@@ -16,3 +17,23 @@ def back():
             ans.pop()            
             
 back()
+"""
+
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+ans = []
+
+def dfs(n, lst):
+    global ans    
+    # 종료조건
+    if n > N:
+        if len(lst) == M:
+            ans.append(lst)
+        return
+    dfs(n+1, lst+[n])
+    dfs(n+1, lst)
+
+dfs(1, [])
+for lst in ans:
+    print(*lst)
