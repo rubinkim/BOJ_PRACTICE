8e7cac3e0eb8a077a1e9032a80cd6ab933111deb
rubinkim <kimyongha2011@gmail.com>
2024-02-09 09:03:15 +0900

    more on 1182

---
+++ BACKTRACKING/1182_부분수열의합.py
@@ -0,0 +1,32 @@
+#-*- coding: utf-8 -*-
+
+"""
+5 0
+-7 -3 -2 5 8
+
+7 0
+9 1 -7 -3 -2 5 8 
+
+5 6
+1 2 3 4 5
+"""
+
+#import sys
+#input = sys.stdin.readline
+N, S = map(int, input().split())
+lst = list(map(int, input().split()))
+ans = 0
+
+def dfs(n, sm, cnt):
+    global ans
+    #종료조건
+    if n == N:
+        if sm == S and cnt > 0:
+            ans += 1
+        return
+    #하부함수 호출
+    dfs(n+1, sm+lst[n], cnt+1)  #lst[n]을 포함시키는 경우
+    dfs(n+1, sm, cnt)           #lst[n]을 포함시키지 않는 경우
+
+dfs(0, 0, 0)
+print(ans)
