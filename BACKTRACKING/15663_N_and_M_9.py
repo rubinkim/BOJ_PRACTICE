78f2bb95c6c433b9fc430ec270fe877036568074
rubinkim <kimyongha2011@gmail.com>
2024-02-10 09:18:52 +0900

    15663_N_and_M_9.py

--- BACKTRACKING/15663_N_and_M_9.py
+++ BACKTRACKING/15663_N_and_M_9.py
@@ -12,4 +12,19 @@ import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
+v = [0] * (N+1)
 ans = []
+
+def dfs(n, lst):
+    if n == M:
+        ans.append(lst)
+        return
+    for j in range(N):
+        if v[j] == 0:
+            v[j] = 1
+            dfs(n+1, lst+[nums[j]])
+            v[j] = 0
+            
+dfs(0, [])
+for lst in ans:
+    print(*lst)
