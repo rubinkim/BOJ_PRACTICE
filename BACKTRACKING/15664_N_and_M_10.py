e9a1db1716c77a82f15665bf300576870e7ebd2e
rubinkim <kimyongha2011@gmail.com>
2024-02-11 14:27:05 +0900

    more on N_and_M

--- BACKTRACKING/15664_N_and_M_10.py
+++ BACKTRACKING/15664_N_and_M_10.py
@@ -3,7 +3,6 @@
 """
 N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오. N개의 자연수는 모두 다른 수이다.
 N개의 자연수 중에서 M개를 고른 수열
-같은 수를 여러 번 골라도 된다.
 고른 수열은 비내림차순이어야 한다.
 길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
 
@@ -11,23 +10,34 @@ N개의 자연수 중에서 M개를 고른 수열
 9 8 7 1
 
 4 2
-9 8 9 1
+9 7 9 1
 """
 
-import sys
-input = sys.stdin.readline
+import time
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
-ans = []
+
+start_time = time.time()
+v = [0] * (N+1)
+ans = set()
 
 def dfs(n, lst):
+    global ans
     if n == M:
-        ans.append(lst)
+        lst = tuple(lst)
+        ans.add(lst)
         return
     for j in range(N):
-        if all([nums[j] >= x for x in lst]):
-            dfs(n+1, lst+[nums[j]])
+        if v[j] == 0:
+            v[j] = 1
+            dfs(n+1, list(lst)+[nums[j]])
+            v[j] = 0
             
-dfs(0, [])
+dfs(0, ())
+intermediate_time = time.time()
+print(f"#1_time_elapsed : {intermediate_time - start_time}")
+
 for lst in sorted(ans):
     print(*lst)
+end_time = time.time()
+print(f"#2_time elapsed : {end_time - start_time}")
