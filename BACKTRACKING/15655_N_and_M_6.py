3f9301eb023b30a2bda82ac0ca4c68ab8f94e894
rubinkim <kimyongha2011@gmail.com>
2024-02-11 18:37:13 +0900

    reviewing all kinds of N_and_M

--- BACKTRACKING/15655_N_and_M_6.py
+++ BACKTRACKING/15655_N_and_M_6.py
@@ -7,22 +7,23 @@
 
 import sys
 input = sys.stdin.readline
-N, M = map(int, input().split()) 
-nums = list(map(int, input().split()))    
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+v = [0] * N
 ans = []
 
 def dfs(n, lst):
     global ans
-    v = [0] * (N+1)
     if n == M:
         ans.append(lst)
         return
-    for j in range(N):            
-        if v[j] == 0 and all([nums[j] > x for x in lst]): 
+    for j in range(N):
+        if v[j] == 0 and all([nums[j]>x for x in lst]):
             v[j] = 1
-            dfs(n+1, lst + [nums[j]])
+            dfs(n+1, lst+[nums[j]])
             v[j] = 0
 
 dfs(0, [])
-for lst in sorted(ans):
+for lst in ans:
     print(*lst)
