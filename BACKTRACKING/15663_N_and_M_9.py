53ac7fa436c7da9b0e5d40c67e5f2f7be12d790b
rubinkim <kimyongha2011@gmail.com>
2024-02-12 07:12:39 +0900

    more review on N_and_Ms

--- BACKTRACKING/15663_N_and_M_9.py
+++ BACKTRACKING/15663_N_and_M_9.py
@@ -12,25 +12,3 @@ N개의 자연수 중에서 M개를 고른 수열
 """
 
 # nums를 정의하고 그다음줄에 nums.sort()를 하고 나서 밑의 loop에서 그냥 ans로 하면 틀리게 된다!!!!!
-import sys
-input = sys.stdin.readline
-N, M = map(int, input().split())
-nums = list(map(int, input().split()))
-v = [0] * (N+1)
-ans = set()
-
-def dfs(n, lst):
-    global ans
-    if n == M:
-        lst = tuple(lst)
-        ans.add(lst)
-        return
-    for j in range(N):
-        if v[j] == 0:
-            v[j] = 1
-            dfs(n+1, list(lst)+[nums[j]])
-            v[j] = 0
-            
-dfs(0, ())
-for lst in sorted(ans):
-    print(*lst)
