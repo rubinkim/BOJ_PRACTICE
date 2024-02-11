93c7feafa1aa88f55b684b33c836a909deee8ec2
rubinkim <kimyongha2011@gmail.com>
2024-02-11 10:39:10 +0900

    15649_N_and_M_1.py

--- BACKTRACKING/15650_N_and_M_2.py
+++ BACKTRACKING/15650_N_and_M_2.py
@@ -3,21 +3,6 @@
 4 2
 """
 """
-n, m = map(int, input().split())
-ans = []
-
-def back():
-    if len(ans) == m:
-        print(" ".join(map(str, ans)))
-    for i in range(1, n+1):
-        if i not in ans and all(i - x > 0 for x in ans):
-            ans.append(i)
-            back()
-            ans.pop()            
-            
-back()
-"""
-"""
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
