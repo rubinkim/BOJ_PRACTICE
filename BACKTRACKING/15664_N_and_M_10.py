5de2189011434dbbaf3dfbdf3c7f01ab5b98c1b8
rubinkim <kimyongha2011@gmail.com>
2024-02-12 11:52:30 +0900

    more review on N_and_M

--- BACKTRACKING/15664_N_and_M_10.py
+++ BACKTRACKING/15664_N_and_M_10.py
@@ -46,6 +46,7 @@ import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
+nums.sort()
 ans = set()
 
 def dfs(n, num_lst, lst):
@@ -58,5 +59,5 @@ def dfs(n, num_lst, lst):
     dfs(n+1, num_lst, lst)
 
 dfs(0, nums, [])
-for lst in sorted(ans):
+for lst in ans:
     print(*lst)
