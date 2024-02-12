370710258634ad2e6ce6e489f08660623949989a
rubinkim <kimyongha2011@gmail.com>
2024-02-12 12:00:01 +0900

    more on N_and_Ms

--- BACKTRACKING/15664_N_and_M_10.py
+++ BACKTRACKING/15664_N_and_M_10.py
@@ -42,3 +42,25 @@ end_time = time.time()
 print(f"#2_time elapsed : {end_time - start_time}")
 """
 
+# 방법 1 : n은 선택할 숫자의 개수이고 j가 0부터 N-1까지 순회한다.
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+ans = set()
+v = [0] * N
+
+def dfs(n, lst):
+    global ans
+    if n == M:
+        ans.add(tuple(lst))
+        return
+    for j in range(N):
+        if v[j] == 0 and all([nums[j] >= x for x in lst]):
+            v[j] = 1
+            dfs(n+1, lst+[nums[j]])
+            v[j] = 0
+
+dfs(0, [])
+for lst in sorted(ans):
+    print(*lst)
