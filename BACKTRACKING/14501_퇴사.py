e4976e7c1d532bb7fc794a5734149d32cca1ec45
rubinkim <kimyongha2011@gmail.com>
2024-02-13 18:55:47 +0900

    more on 14501

--- BACKTRACKING/14501_퇴사.py
+++ BACKTRACKING/14501_퇴사.py
@@ -45,7 +45,7 @@ def dfs(n, pay):     # n : T배열의 인덱스
         #print(f"n : {n},  pay : {pay}")
         pay_lst.append(pay)
         return
-    if n + T[n] <= T[-1]:
+    if n + T[n] <= len(T):
         #dfs(n+T[n], pay+P[n])
         #dfs(n+T[n], pay)  
         print(f"n : {n}, T[n] : {T[n]},  P[n] : {P[n]}")
