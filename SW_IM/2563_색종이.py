918699219fcadce1a4e0ebba5d5a718cccd07681
rubinkim <kimyongha2011@gmail.com>
2024-02-18 19:35:03 +0900

    2309_7dwarfs.py

--- SW_IM/2563_색종이.py
+++ SW_IM/2563_색종이.py
@@ -12,4 +12,9 @@
 from itertools import combinations
 import sys
 input = sys.stdin.readline
-
+N = int(input())
+lst = []
+for _ in range(N):
+    lst.append(list(int, input().split()))
+    
+print(lst)
