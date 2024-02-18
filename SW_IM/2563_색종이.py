95736941550f4a3b9b739b712c771a34bf4a9f93
rubinkim <kimyongha2011@gmail.com>
2024-02-18 19:39:17 +0900

    2309_7dwarfs.py

--- SW_IM/2563_색종이.py
+++ SW_IM/2563_색종이.py
@@ -14,7 +14,13 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 lst = []
+overlapped = 0
+
 for _ in range(N):
     lst.append(list(map(int, input().split())))
     
 print(lst)
+
+for area0, area1 in combinations(lst, 2):
+    if abs(area0[0] - area1[0]) < 10 and  abs(area0[1] - area1[1]) < 10:
+        
