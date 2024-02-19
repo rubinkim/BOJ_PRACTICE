dc9aa4ca087980b1ba92e2f4b4e1d2a829dfbd97
rubinkim <kimyongha2011@gmail.com>
2024-02-19 14:43:04 +0900

    more on 2563.py

--- SW_IM/2563_색종이.py
+++ SW_IM/2563_색종이.py
@@ -29,8 +29,7 @@ for (x1, y1), (x2, y2) in combinations(lst, 2):
             overlapped += (x2 + 10 - x1) * (y1 + 10 - y2)
         elif (x1 < x2) and (y1 >= y2):
             overlapped += (x1 + 10 - x2) * (y2 + 10 - y1)
-            print(overlapped)
         elif (x1 < x2) and (y1 < y2):
             overlapped += (x1 + 10 - x2) * (y1 + 10 - y2)
 
-print(overlapped)
+print(N * 100 - overlapped)
