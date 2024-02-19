26554245095f1ec0f91c13621ea376121e081d86
rubinkim <kimyongha2011@gmail.com>
2024-02-19 15:59:27 +0900

    more on 2563.py

--- SW_IM/2563_색종이.py
+++ SW_IM/2563_색종이.py
@@ -9,27 +9,5 @@
 260
 """
 
-from itertools import combinations
-import sys
-input = sys.stdin.readline
 N = int(input())
-lst = []
-overlapped = 0
-
-for _ in range(N):
-    lst.append(list(map(int, input().split())))
-    
-#print(lst)
-
-for (x1, y1), (x2, y2) in combinations(lst, 2):
-    if abs(x1 - x2) <= 10 and abs(y1 - y2) <= 10:
-        if (x1 >= x2) and (y1 >= y2):
-            overlapped += (x2 + 10 - x1) * (y2 + 10 - y1)
-        elif (x1 >= x2) and (y1 < y2):
-            overlapped += (x2 + 10 - x1) * (y1 + 10 - y2)
-        elif (x1 < x2) and (y1 >= y2):
-            overlapped += (x1 + 10 - x2) * (y2 + 10 - y1)
-        elif (x1 < x2) and (y1 < y2):
-            overlapped += (x1 + 10 - x2) * (y1 + 10 - y2)
-
-print(N * 100 - overlapped)
+arr = [[0] * 102 for _ in range(102)]
