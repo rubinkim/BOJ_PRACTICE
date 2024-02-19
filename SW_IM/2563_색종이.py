7141ae30fe09624d14ce5ba546f04f8355cd662b
rubinkim <kimyongha2011@gmail.com>
2024-02-19 14:08:07 +0900

    more on 2563.py

--- SW_IM/2563_색종이.py
+++ SW_IM/2563_색종이.py
@@ -22,14 +22,6 @@ for _ in range(N):
 print(lst)
 
 for area0, area1 in combinations(lst, 2):
-    if abs(area0[0] - area1[0]) < 10 and  abs(area0[1] - area1[1]) < 10:
-        if area0[0] <= area1[0] and area0[1] <= area1[1]:
-            overlapped += abs(area0[0]+10 - area1[0]) * abs(area0[1]+10 - area1[1])
-        elif area0[0] > area1[0] and area0[1] <= area1[1]:
-            overlapped += abs(area1[0]+10 - area0[0]) * abs(area0[1]+10 - area1[1])
-        elif area0[0] <= area1[0] and area0[1] > area1[1]:
-            overlapped += abs(area0[0]+10 - area1[0]) * abs(area1[1]+10 - area0[1])
-        elif area0[0] > area1[0] and area0[1] > area1[1]:
-            overlapped += abs(area1[0]+10 - area0[0]) * abs(area1[1]+10 - area0[1])
+
 
 print(3 * 100 - overlapped)
