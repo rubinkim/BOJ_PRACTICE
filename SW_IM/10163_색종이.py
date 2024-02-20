79c8b93159e7a01b1ef6a7f98819c3f72c91aa71
rubinkim <kimyongha2011@gmail.com>
2024-02-20 21:05:57 +0900

    more on 10163.py

--- SW_IM/10163_색종이.py
+++ SW_IM/10163_색종이.py
@@ -27,5 +27,10 @@
 # method 1 : looping arr for every color paper : takes a very long time!
 N = int(input())
 arr = [[0] * 1001 for _ in range(1001)]
-f
+for _ in range(N):
+    sj, si, w, h = map(int, input().split())
+    for i in range(si, si+h):
+        for j in range(sj, sj+w):
+            arr[]
+    
     
