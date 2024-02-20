e797ca7c460361b8a699fe4904340a411eec34c9
rubinkim <kimyongha2011@gmail.com>
2024-02-20 21:01:56 +0900

    more on 10163.py

--- SW_IM/10163_색종이.py
+++ SW_IM/10163_색종이.py
@@ -25,25 +25,5 @@
 62 24 0 120
 """
 
-N = int(input())
-arr = [[0] * 1002 for _ in range(1002)]
 
-for num in range(1, N+1):
-    sj, si, w, h = map(int, input().split())
-    for i in range(si, si+h):
-        for j in range(sj, sj+w):
-            arr[i][j] = num
-            
-nums_lst = []
-
-for x in range(1, N+1):
-    cnt = 0
-    for i in range(1002):
-        for j in range(1002):
-            if arr[i][j] == x:
-                cnt += 1
-    nums_lst.append(cnt)
-    
-for x in nums_lst:
-    print(x)
     
