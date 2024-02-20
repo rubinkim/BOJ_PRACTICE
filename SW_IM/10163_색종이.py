865670ec1c713244f1bdf7c8ffd368088476a17c
rubinkim <kimyongha2011@gmail.com>
2024-02-20 21:16:35 +0900

    more on 10163.py

--- SW_IM/10163_색종이.py
+++ SW_IM/10163_색종이.py
@@ -39,5 +39,20 @@ for n in range(1, N+1):
         cnt += lst.count(n)
     print(cnt)
             
+# method 2 : Using frequenct list and then loop the arr just once! : can save a lot of time!!!            
+N = int(input())
+arr = [[0] * 1001 for _ in range(1001)]
+for n in range(1, N+1):
+    sj, si, w, h = map(int, input().split())
+    for i in range(si, si+h):
+        for j in range(sj, sj+w):
+            arr[i][j] = n
+            
+cnts = [0] * (N+1)
+for lst in arr:
+    for n in lst:
+        cnts[n] += 1
+print(*cnts[1:], sep='\n')
+            
     
     
