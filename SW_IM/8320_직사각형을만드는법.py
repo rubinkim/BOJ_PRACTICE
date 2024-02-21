f445977166122feb76e1b37f9510cd3d1902405a
rubinkim <kimyongha2011@gmail.com>
2024-02-21 14:15:35 +0900

    8320_creating_rectagles.py

--- SW_IM/8320_직사각형을만드는법.py
+++ SW_IM/8320_직사각형을만드는법.py
@@ -10,7 +10,7 @@ N = int(input())
 cnt = 0
 for n in range(1, N+1):
     for i in range(n, int(N/n) + 1):
-        if n % i == 0:
+        if n * i <= N:
             cnt += 1
             
 print(cnt)
