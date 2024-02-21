e0179227f235f22e7f22531c4a836ab42f7df4e0
rubinkim <kimyongha2011@gmail.com>
2024-02-21 21:07:15 +0900

    swea_4613_러시아 국기 같은 깃발.py

--- SW_IM/SWEA_4613_러시아 국기 같은 깃발.py
+++ SW_IM/SWEA_4613_러시아 국기 같은 깃발.py
@@ -1,17 +1,17 @@
 #-*- coding: utf-8 -*-
 
 """
-6
-
-8
-
-# 1. 가능한 모든 경우를 순회 : 이중 for문을 이용한다.
-N = int(input())
-cnt = 0
-for n in range(1, N+1):
-    for i in range(n, int(N/n) + 1):
-        if n * i <= N:
-            cnt += 1
-            
-print(cnt)
+2
+4 5
+WRWRW
+BWRWB
+WRWRW
+RWBWR
+6 14
+WWWWWWWWWWWWWW
+WWRRWWBBBBBBWW
+WRRRWWWBWWWWRB
+WWBWBWWWBWRRRR
+WBWBBWWWBBWRRW
+WWWWWWWWWWWWWW
 """
