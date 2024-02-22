aadc543682e86835f8f1cc727aadb18751860566
rubinkim <kimyongha2011@gmail.com>
2024-02-22 14:00:23 +0900

    swea_4613.py

--- SW_IM/SWEA_4613_러시아 국기 같은 깃발.py
+++ SW_IM/SWEA_4613_러시아 국기 같은 깃발.py
@@ -51,7 +51,7 @@ def count_color(w, b, n, m):
 print(count_color(1, 2, N1, M1))
 
 cnt_lst = []
-for a in range(3):
-    for b in range(2):
+for x in range(3):
+    for y in range(2):
         #print(f"i : {i},  j : {j},  cnt : {count_color(i, j, N1, M1)}")
-        print(count_color(a, b, N1, M1))
+        print(count_color(x, y, N1, M1))
