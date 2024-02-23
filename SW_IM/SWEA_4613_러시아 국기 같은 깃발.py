4828b1175382210a42fef64e32fb6a8072bc8740
rubinkim <kimyongha2011@gmail.com>
2024-02-23 13:22:31 +0900

    more on swea_4613.py

--- SW_IM/SWEA_4613_러시아 국기 같은 깃발.py
+++ SW_IM/SWEA_4613_러시아 국기 같은 깃발.py
@@ -49,4 +49,7 @@ for tc in range(1, T+1):
                 arr[s].count('W')
             for s in range(i+1, j+1):
                 arr[s].count('B')    
+            for s in range(j+1, N):
+                arr[s].count('R')
+            
 
