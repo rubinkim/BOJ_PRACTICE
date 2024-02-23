7540a1250e7f5fe93e74c7750dbc96e1b03d117a
rubinkim <kimyongha2011@gmail.com>
2024-02-24 05:38:52 +0900

    2605_줄세우기.py

--- SW_IM/2628_종이자르기.py
+++ SW_IM/2628_종이자르기.py
@@ -10,7 +10,7 @@
 30
 """
 
-h, v = map(int, input().split())          # h : 가로축의 길이(j의 최대값),  v : 세로축의 길이(i의 최대값)
+h, v = map(int, input().split())          # h : h_axis(가로축)의 길이(j의 최대값),  v : v_axis(세로축)의 길이(i의 최대값)
 n = int(input())                          # n : 잘라야 하는 점선의 개수
 h_axis, v_axis = [0, h], [0, v]           # v_axis : i들을 저장하는 리스트(세로축), h_axis : j들을 저장하는 리스트(가로축)
 for _ in range(n):
@@ -21,13 +21,13 @@ for _ in range(n):
         h_axis.append(x)
 
 h_axis.sort()
-verticals.sort()
+v_axis.sort()
 
-print(horizontals)
-print(verticals)
+print(h_axis)
+print(v_axis)
 
 mx_area = 0
-for i in range(1, len(horizontals)):
-    for j in range(1, len(verticals)):
-        mx_area = max(mx_area, (horizontals[i] - horizontals[i-1]) * (verticals[j] - verticals[j-1]))
+for i in range(1, len(v_axis)):
+    for j in range(1, len(h_axis)):
+        mx_area = max(mx_area, (v_axis[i] - v_axis[i-1]) * (h_axis[j] - h_axis[j-1]))
 print(mx_area)
