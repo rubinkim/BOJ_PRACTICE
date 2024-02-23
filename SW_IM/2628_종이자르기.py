cbbb347b4737eccd343918a4a34743dcd56fc845
rubinkim <kimyongha2011@gmail.com>
2024-02-23 14:01:46 +0900

    more on swea_4613.py

--- SW_IM/2628_종이자르기.py
+++ SW_IM/2628_종이자르기.py
@@ -12,3 +12,4 @@
 
 h, v = map(int, input().split())       # h : 가로,  v : 세로
 n = int(input())                       # n : 잘라야 하는 점선의 개수
+horizontals, verticals = [], []        # horizontals : 가로로 자르는 i들을 저장하는 리스트, verticals : 세로로 자르는 j들을 저장하는 리스트
