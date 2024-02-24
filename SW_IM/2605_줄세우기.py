9117f2247f50abb6dbb7b7c1e25b3543033756f1
rubinkim <kimyongha2011@gmail.com>
2024-02-24 11:25:33 +0900

    2605_줄세우기.py

--- SW_IM/2605_줄세우기.py
+++ SW_IM/2605_줄세우기.py
@@ -16,5 +16,10 @@ head, current, pre = None, None, None
 
 # 클래스와 함수 선언 부분
 class Node():
-    
+    def __init__(self):
+        self.data = None
+        self.link = None
+        
+    def printNodes(start):
+        
     
