b989bca0cec5152452aedbbffcf61ed39e1d221c
rubinkim <kimyongha2011@gmail.com>
2024-02-26 18:58:28 +0900

    more on 10431.py

--- SW_IM/10431_줄세우기.py
+++ SW_IM/10431_줄세우기.py
@@ -36,7 +36,7 @@ for x in range(1, P+1):
             break
         
     print(x, ans)
-"""    
+    
 
 P = int(input())
 tc_dict = {}
@@ -56,3 +56,13 @@ for x in range(1, P+1):
                 tc_dict[x][j] = temp
                 break
     print(x, ans)
+"""
+P = int(input())
+for tc in range(1, P+1):
+    lst = list(map(int, input().split()))[1:]
+    cnt = 0
+    for i in range(1,20):
+        for j in range(i):
+            if lst[i] < lst[j]:
+                cnt += 1
+        print(f"{tc} {cnt}")
