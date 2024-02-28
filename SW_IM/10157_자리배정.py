e9cbc2ae1b8533b34ba34212133bd78869d9dfe2
rubinkim <kimyongha2011@gmail.com>
2024-02-28 17:17:36 +0900

    more on 3985.py

--- SW_IM/10157_자리배정.py
+++ SW_IM/10157_자리배정.py
@@ -22,7 +22,7 @@ K = int(input())
 board = [[0] * C for _ in range(R)]
 for i in range(0, R):
     for j in range(0, C):
-        board[i][j] = (C-j, R-i)
+        board[i][j] = (j+1, R-i)
         
 for row in board:
     print(row)
