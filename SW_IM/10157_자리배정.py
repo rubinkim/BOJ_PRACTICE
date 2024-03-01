e1034eeeb980fafdaf34c098c59d3bcb19f63ce0
rubinkim <kimyongha2011@gmail.com>
2024-03-01 19:20:14 +0900

    10157.py

--- SW_IM/10157_자리배정.py
+++ SW_IM/10157_자리배정.py
@@ -63,5 +63,7 @@ else:
         if arr[ni][nj] == 0:
             ci, cj = ni, nj
         # 범위 밖에 있거나 또는 이미 숫자로 채워져 있으면 방향꺽기
-        else        
+        else:
+            dr = (dr + 1) % 4
+            ci = ci + di[dr]        
     
