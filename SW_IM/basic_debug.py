4e8deb35753f036672f9e52c4bcfe328db57ce29
rubinkim <kimyongha2011@gmail.com>
2024-03-02 01:44:47 +0900

    basic_debug.py

--- SW_IM/basic_debug.py
+++ SW_IM/basic_debug.py
@@ -1,1 +1,13 @@
 #-*- coding: utf-8 -*-
+
+def find_dog(sound):
+    if sound == "멍멍":
+        return("개가 짖네")
+
+    else:
+        return("다른 동물이구나")
+
+sound = "야옹"
+find_result = find_dog(sound)
+
+print(find_result)
