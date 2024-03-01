25d7cf4c0dcd4d22e0c2b4cfc3928e16e3f33c3d
rubinkim <kimyongha2011@gmail.com>
2024-03-01 19:14:37 +0900

    10157.py

--- SW_IM/10157_자리배정.py
+++ SW_IM/10157_자리배정.py
@@ -56,5 +56,7 @@ else:
     arr = [[1] * (C+2)] + [[1] + [0] * C + [1] for _ in range(R)] + [[1] * (C+2)]
     ci, cj, dr = 1, 1, 0
     
-    
+    for n in range(1, K):           # 관객들에게 배정된 번호표
+        
+        
     
