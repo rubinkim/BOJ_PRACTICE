514acdba6780857e27bc029d631a73385e850fa0
rubinkim <kimyongha2011@gmail.com>
2024-03-04 16:03:31 +0900

    more on bingo.py

--- SW_IM/2578_빙고.py
+++ SW_IM/2578_빙고.py
@@ -13,41 +13,7 @@
 11 24 9 20 15
 """
 
-bingo = []
-calls = []
 
-
-for i in range(10):    
-    if i < 5:
-        bingo.append(list(map(int, input().split())))
-    else:
-        calls.extend(list(map(int, input().split())))        
-
-d0 = [bingo[i][i] for i in range(5)]
-d1 = [bingo[4-i][i] for i in range(5)]
-
-cnt = 0
-min_idx = []
-
-for x in calls:
-    for i in range(5):
-        for j in range(5):
-            if bingo[i][j] == x:
-                bingo[i][j] = 0
-                if sum(bingo[i]) == 0:
-                    cnt += 1                    
-                if sum([bingo[x][j] for x in range(5)]) == 0:
-                    cnt += 1
-                if (i,j) in [(0,0), (1,1), (2,2), (3,3), (4,4)] and sum([bingo[k][k] for k in range(5)]) == 0:
-                    cnt += 1
-                if (i,j) in [(4,0), (3,1), (2,2), (1,3), (0,4)] and sum([bingo[4-k][k] for k in range(5)]) == 0:
-                    cnt += 1
-                if cnt == 3:
-                    #print(f"x : {x},  cnt : {cnt}, x의 인덱스 : {calls.index(x) + 1}")
-                    #for row in bingo:
-                        #print(row)
-                    min_idx.append(calls.index(x)+1)
-print(min(min_idx))
 
         
 
