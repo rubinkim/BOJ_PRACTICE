ecad3995d2217395eb3aa0fc74aa5752344760f6
rubinkim <kimyongha2011@gmail.com>
2024-03-06 17:15:53 +0900

    10709_weather_caster.py

--- SW_IM/10709_기상캐스터.py
+++ SW_IM/10709_기상캐스터.py
@@ -32,3 +32,10 @@ v = [[0] * W for _ in range(H)]
 
 for i in range(H):
     cnt = -1
+    for j in range(W):
+        if arr[i][j] == 'c':
+            cnt = 0
+        else:
+            if cnt >= 0:
+                cnt += 1
+        v[i][j] = cnt
