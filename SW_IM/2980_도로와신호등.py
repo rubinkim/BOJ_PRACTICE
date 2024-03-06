94e99b629353a4a8de117baafb160556c47732e9
rubinkim <kimyongha2011@gmail.com>
2024-03-06 19:58:10 +0900

    2980_도로와신호등.py

--- SW_IM/2980_도로와신호등.py
+++ SW_IM/2980_도로와신호등.py
@@ -25,8 +25,9 @@ t = 0
 
 for i in range(1, len(drg)):
     t += drg[i][0] - drg[i-1][0]
-    if t - (t // (drg[i][1] + drg[i][2])) * (drg[i][1] + drg[i][2]) <= drg[i][1]:
-        t += (drg[i][1] -t) 
+    print(f"i : {i},  leftover : {(t // (drg[i][1] + drg[i][2])) * (drg[i][1] + drg[i][2])},  red : {drg[i][1]}")
+    if t - (t // (drg[i][1] + drg[i][2])) * (drg[i][1] + drg[i][2]) < drg[i][1]:        
+        t = t + (drg[i][1] -t) 
         
     #print(f"i : {i}    t : {t}")
 
