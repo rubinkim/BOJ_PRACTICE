bd2b76ff7a409aa5c1b4c2f8b98094a203f756e8
rubinkim <kimyongha2011@gmail.com>
2024-03-07 17:59:10 +0900

    2559_수열.py

--- SW_IM/2559_수열.py
+++ SW_IM/2559_수열.py
@@ -10,7 +10,6 @@
 3 -2 -4 -9 0 3 7 13 8 -3
 
 31
-"""
 
 N, K = map(int, input().split())
 lst = list(map(int, input().split()))
@@ -18,4 +17,16 @@ lst = list(map(int, input().split()))
 max_val = -1001
 for i in range(len(lst) - K + 1):
     max_val = max(max_val, sum([x for x in lst[i:i+K]]))
+print(max_val)
+"""
+
+N, K = map(int, input().split())
+lst = list(map(int, input().split()))
+
+max_val = -1001
+for i in range(len(lst) - K + 1):
+    if i == 0:
+        max_val = max(max_val, sum([x for x in lst[i:i+K]]))
+    else:
+        max_val = max(max_val, max_val - lst[i-1] + lst[i+k])
 print(max_val)
