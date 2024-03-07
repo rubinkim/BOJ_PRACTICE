eb44171eaa0a7e60b6714551131090f6cd21094e
rubinkim <kimyongha2011@gmail.com>
2024-03-07 14:25:50 +0900

    2839_설탕배달.py

--- SW_IM/2839_설탕배달.py
+++ SW_IM/2839_설탕배달.py
@@ -7,7 +7,7 @@ N = int(input())
 cnt5 = N // 5
 cnt3 = (N - cnt5 * 5) // 3
 
-while cnt5 >= 5:
+while cnt5 >= 0:
     cnt3 = (N - cnt5 * 5) // 3
     if cnt5 * 5 + cnt3 * 3 == N:
         print(cnt5 + cnt3)
