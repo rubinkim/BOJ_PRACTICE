cf4c171a057d6a3f59b3cff7a3514b32f6556a64
rubinkim <kimyongha2011@gmail.com>
2024-03-08 15:03:35 +0900

    1063_king.py

--- SW_IM/1063_킹.py
+++ SW_IM/1063_킹.py
@@ -19,9 +19,9 @@ print(f"king_x : {king_x},  king_y : {king_y},  stone_x : {stone_x},  stone_y : 
 board = [[0] * 8 for _ in range(8)]
 row = '87654321'
 col = 'ABCDEFGH'
-for j in range(len(col)):
-    for i in range(len(row)):
-        board[i][j] = col[i] + row[j]
+for i in range(len(row)):
+    for j in range(len(col)):
+        board[i][j] = col[j] + row[i]
 
 for row in board:
     print(row)
