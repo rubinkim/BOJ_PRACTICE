e8cf6bb29e1546aff6f6b76977dbaf2098d9a4be
rubinkim <kimyongha2011@gmail.com>
2024-03-08 20:32:37 +0900

    more on 1063.py

--- SW_IM/1063_킹.py
+++ SW_IM/1063_킹.py
@@ -66,26 +66,6 @@ LB
 
 king, stone, N = input().split()
 N = int(N)
-print(f"king : {king},  stone : {stone},  N : {N}")
-
-board = [[0] * 8 for _ in range(8)]
-row = '87654321'
-col = 'ABCDEFGH'
-for i in range(len(row)):
-    for j in range(len(col)):
-        board[i][j] = col[j] + row[i]
-
-for row in board:
-    print(row)
-    
-directions = ['R', 'L', 'B', 'T', 'RT', 'LT', 'RB', 'LB']
-moves = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, 1), (-1, -1), (1, 1), (1, -1)]
-
-moves_dict = {}
-for k, v in zip(directions, moves):
-    moves_dict[k] = v
-    
-print(moves_dict)
 
 def convert_to_idx(x):
     return (8 - int(x[1]), ord(x[0]) - 65)
@@ -94,6 +74,20 @@ def convert_to_position(y, x):
     new_y = str(8 - y)
     new_x = chr(x + 65)
     return new_x + new_y 
+
+board = [[0] * 8 for _ in range(8)]
+row = '87654321'
+col = 'ABCDEFGH'
+for i in range(len(row)):
+    for j in range(len(col)):
+        board[i][j] = col[j] + row[i]
+    
+directions = ['R', 'L', 'B', 'T', 'RT', 'LT', 'RB', 'LB']
+moves = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, 1), (-1, -1), (1, 1), (1, -1)]
+
+moves_dict = {}
+for k, v in zip(directions, moves):
+    moves_dict[k] = v
 
 king_y, king_x = convert_to_idx(king)
 stone_y, stone_x = convert_to_idx(stone)
@@ -110,10 +104,9 @@ for _ in range(N):
             king_y += dy
             king_x += dx
             stone_y += dy
-            stone_y += dx
+            stone_x += dx
         if king_y + dy == stone_y and king_x + dx == stone_x and (stone_y + dy < 0 or stone_y + dy >= 8 or stone_x + dx < 0 or stone_x + dx >= 8):
-            continue
-        
+            continue        
 
 print(convert_to_position(*(king_y, king_x)))
 print(convert_to_position(*(stone_y, stone_x)))
