016d50c92d57aff1c6cd6121c50909e8266b7a9a
rubinkim <kimyongha2011@gmail.com>
2024-03-10 21:05:40 +0900

    2304_창고다각형.py

--- SW_IM/2304_창고다각형.py
+++ SW_IM/2304_창고다각형.py
@@ -38,11 +38,11 @@ for i in range(len(L)):
     if H[current_idx] == max(H[current_idx:]):
         next_idx = H.index(max(H[current_idx+1:]))
         area += H[current_idx] + (next_idx - current_idx - 1) * H[next_idx]
-    elif H[current_idx] < max(H[current_idx:]) and H[current_idx] <= H[current_idx + 1]:
-        area += H[current_idx] * (L[current_idx + 1] - L[current_idx])
     elif H[current_idx] < max(H[current_idx:]) and H[current_idx] > H[current_idx + 1]:
         next_highter_idx = [i for i in range(current_idx+1, len(H)) if H[i] >= H[current_idx]][0]
         area += H[current_idx] * (L[next_highter_idx] - L[current_idx])
+    elif H[current_idx] < max(H[current_idx:]) and H[current_idx] <= H[current_idx + 1]:
+        area += H[current_idx] * (L[current_idx + 1] - L[current_idx])
         
 print(area)
 
