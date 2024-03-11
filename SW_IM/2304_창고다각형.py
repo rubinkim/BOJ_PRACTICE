a5d0f3c8f73bb92db01c050b5a1a0d8556a08f9a
rubinkim <kimyongha2011@gmail.com>
2024-03-11 15:41:51 +0900

    more on 2304.py

--- SW_IM/2304_창고다각형.py
+++ SW_IM/2304_창고다각형.py
@@ -19,35 +19,28 @@ for _ in range(N):
     lst.append((left, height))
     
 lst.sort(key=lambda x : x[0])
-print(lst)
 
 L, H = [], []
 for l, h in lst:
     L.append(l)
     H.append(h)
     
-print(f"L : {L}")
-print(f"H : {H}")
-
 area = 0
 current_idx = 0
 
 while current_idx <= len(H)-1:
     if current_idx == len(H)-1:
         break
-    print(f"{H[current_idx]}, {[H[x] for x in range(current_idx+1, N)]}")
+
     if any([H[x] > H[current_idx] for x in range(current_idx+1, N)]):
         first_idx = [x for x in range(current_idx+1, N) if H[x] > H[current_idx]][0]
-        print(f"first_idx : {first_idx}")
 
         area += (L[first_idx] - L[current_idx]) * H[current_idx]
-        print(f"current_idx : {current_idx}, area : {area}")
         current_idx = first_idx
         
     elif all([H[current_idx] > H[x] for x in range(current_idx+1, N)]):
         first_idx = H.index(max(H[current_idx+1 : N]))
         area = area + H[current_idx] * 1 + (L[first_idx] - L[current_idx]) * H[first_idx]
-        print(f"current_idx : {current_idx}, area : {area}")
         current_idx = first_idx
         
 print(area)
