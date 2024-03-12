e897514ea00ff3124d1a4d5ca7677c3956715d63
rubinkim <kimyongha2011@gmail.com>
2024-03-12 14:24:27 +0900

    2304_창고다각형.py

--- SW_IM/2304_창고다각형.py
+++ SW_IM/2304_창고다각형.py
@@ -12,52 +12,7 @@
 98
 """
 
-N = int(input())
-lst = []
-for _ in range(N):
-    left, height = map(int, input().split())
-    lst.append((left, height))
-    
-lst.sort(key=lambda x : x[0])
 
-#print(lst)
-
-mx_i = 0
-mx = 0
-
-for i in range(N):
-    if lst[i][1] > mx:
-        mx = max(mx, lst[i][1])
-        mx_i = i
-        
-#print(f"mx_i : {mx_i},  mx : {mx}")
-
-# 오른쪽 면적 계산
-area = 0
-current_left = lst[0][0]
-current_height = lst[0][1]
-
-for i in range(1, mx_i+1):       # mx까지 포함
-    if lst[i][1] > current_height:
-        previous_left, previous_height = current_left, current_height
-        current_left, current_height = lst[i][0], lst[i][1]
-        area += (current_left - previous_left) * previous_height
-        #print(f"i : {i},  previous_left : {previous_left},  current_left : {current_left},  previous_height : {previous_height},  area : {area}")
-    if i == mx_i:
-        area += current_height * 1
-        
-# 왼쪽 면적 계산
-current_left = lst[-1][0]
-current_height = lst[-1][1]
-
-for i in range(N-1, mx_i-1, -1):
-    if lst[i][1] > current_height:
-        previous_left, previous_height = current_left, current_height
-        current_left, current_height = lst[i][0], lst[i][1]
-        area += (previous_left - current_left) * previous_height
-        #print(f"i : {i},  previous_left : {previous_left},  current_left : {current_left},  previous_height : {previous_height},  area : {area}")
-        
-print(area)
 
 
 """
