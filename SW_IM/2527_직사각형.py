0f482f6985a85aeb284559feed0f56836252be9b
rubinkim <kimyongha2011@gmail.com>
2024-03-12 20:05:16 +0900

    2527_rectangle.py

--- SW_IM/2527_직사각형.py
+++ SW_IM/2527_직사각형.py
@@ -8,20 +8,22 @@
 
 for _ in range(4):
     x1, y1, p1, q1, x2, y2, p2, q2 = map(int, input().split())
-    print(f"x1 : {x1}, y1 : {y1}, p1 : {p1}, q1 : {q1}, x2 : {x2}, y2 : {y2}, p2 : {p2}, q2 : {q2}")
+    #print(f"x1 : {x1}, y1 : {y1}, p1 : {p1}, q1 : {q1}, x2 : {x2}, y2 : {y2}, p2 : {p2}, q2 : {q2}")
 
     small_x = min(x1, x2)
     small_y = min(y1, y2)
     large_x = max(p1, p2)
     large_y = max(q1, q2)
 
-    print(f"small_x : {small_x}, small_y : {small_y}, large_x : {large_x}, large_y : {large_y}")
+    #print(f"small_x : {small_x}, small_y : {small_y}, large_x : {large_x}, large_y : {large_y}")
 
     if (large_x - small_x) < (p1 - x1) + (p2 - x2) and (large_y - small_y) < (q1 - y1) + (q2 - y2):
         print("a")
-    elif ((large_x - small_x) == (p1 - x1) + (p2 - x2) and (large_y - small_y) < (q1 - y1) + (q2 - y2)) or ((large_x - small_x) < (p1 - x1) + (p2 - x2) and (large_y - small_y) == (q1 - y1) + (q2 - y2)):
+    elif (large_x - small_x) == (p1 - x1) + (p2 - x2) and (large_y - small_y) < (q1 - y1) + (q2 - y2): 
+        print("b")
+    elif (large_x - small_x) < (p1 - x1) + (p2 - x2) and (large_y - small_y) == (q1 - y1) + (q2 - y2):
         print("b")
     elif (large_x - small_x) == (p1 - x1) + (p2 - x2) and (large_y - small_y) == (q1 - y1) + (q2 - y2):
         print("c")
-    elif (large_x - small_x) > (p1 - x1) + (p2 - x2) and (large_y - small_y) > (q1 - y1) + (q2 - y2):
+    elif (large_x - small_x) > (p1 - x1) + (p2 - x2) or (large_y - small_y) > (q1 - y1) + (q2 - y2):
         print("d")
