e28d1f78fa4977294fdf70d9381a407f3dbe663a
rubinkim <kimyongha2011@gmail.com>
2024-03-14 15:36:46 +0900

    13300_방배정.py

--- SW_IM/13300_방배정.py
+++ SW_IM/13300_방배정.py
@@ -28,28 +28,24 @@
 0 6
 
 (3)
+
+7 2
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
+1 1
 """
 
 N, K = map(int, input().split())
 my_dict = {}
 
-for i in range(N):
-    S, Y = map(int, input().split())
-    if (S,Y) not in my_dict:
-        my_dict[(S, Y)] = 1
-    else:
-        my_dict[(S, Y)] += 1
-            
-#print(my_dict)
-
-cnt = 0
-for val in my_dict.values():
-    if val  % 2 == 0:
-        cnt += val // 2
-    else:
-        cnt += val // 2 + 1
+for sex in range(0, 2):
+    for year in range(1, 7):
+        my_dict[(sex, year)] = 0
         
-print(cnt)
-
+print(my_dict)
 
 
