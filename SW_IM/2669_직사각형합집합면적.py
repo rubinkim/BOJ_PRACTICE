6c7e21e958ff93e382402ddf7798ba421679d33a
rubinkim <kimyongha2011@gmail.com>
2024-03-14 17:06:26 +0900

    13300_방배정.py

--- SW_IM/2669_직사각형합집합면적.py
+++ SW_IM/2669_직사각형합집합면적.py
@@ -12,3 +12,9 @@ arr = [[0] * 101 for _ in range(101)]
 cnt = 0
 for _ in range(4):
     x1, y1, x2, y2 = map(int, input().split())
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            if arr[i][j] == 0:
+                arr[i][j] = 1
+                cnt += 1
+print(cnt)
