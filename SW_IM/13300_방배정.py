f1fc8b2ab13aee3a0387ea6e9bba01c6cc43ff40
rubinkim <kimyongha2011@gmail.com>
2024-03-14 15:17:39 +0900

    13300_방배정.py

--- SW_IM/13300_방배정.py
+++ SW_IM/13300_방배정.py
@@ -74,27 +74,4 @@ else:
 print(cnt)
 """
 
-n, k = map(int, input().split())  #학생 수, 한 방 최대 인원 수
-female = [0] * 1000
-male = [0] * 1000
 
-for j in range(n):
-    s, y = map(int, input().split())  #성별, 학년
-    #학년별 인원수 체크
-    if s == 0:  #여자
-        female[y] += 1
-    else:  #남자
-        male[y] += 1
-    
-for j in range(1, 7):
-    if female[j] % k == 0:  #인원수 딱 맞게 떨어질 때
-        female[j] = female[j] // k
-    else:
-        female[j] = (female[j] // k) + 1
-
-    if male[j] % k == 0:
-        male[j] = male[j] // k
-    else:
-        male[j] = (male[j] // k) + 1
-
-print(sum(female) + sum(male))
