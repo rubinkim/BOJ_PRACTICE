4484484b93b49730e291131f5ce0f0dbb6647587
rubinkim <kimyongha2011@gmail.com>
2024-03-15 18:23:42 +0900

    14696_딱지놀이.py

--- SW_IM/1244_스위치켜고끄기.py
+++ SW_IM/1244_스위치켜고끄기.py
@@ -22,18 +22,18 @@ print(k_lst)
 
 def switch_motion(sex, switch_num):
     if sex == 1:
-        for i in range(switch_num, N):
-            if i % switch_num == 0:
+        for i in range(switch_num-1, N):
+            if (i+1) % switch_num == 0:
                 lst[i] = 1 - lst[i]
-    else:
-        lst[switch_num] = 1 - lst[switch_num]
+    elif sex == 2:
+        lst[switch_num-1] = 1 - lst[switch_num-1]
         i = 1
         while True:
-            if lst[switch_num - i] == lst[switch_num + i]:
-                lst[switch_num - i] = 1 - lst[switch_num - i]
-                lst[switch_num + i] = 1 - lst[switch_num + i]
+            if lst[switch_num -1 - i] == lst[switch_num -1 + i]:
+                lst[switch_num -1 - i] = 1 - lst[switch_num -1 - i]
+                lst[switch_num -1 + i] = 1 - lst[switch_num -1 + i]
                 i += 1
-            elif switch_num - i <= -1 or switch_num + i >= N or lst[switch_num - i] != lst[switch_num + i]:
+            elif switch_num -1 - i <= -1 or switch_num -1 + i >= N or lst[switch_num -1 - i] != lst[switch_num -1 + i]:
                 break
             
 for gender, switch_num in k_lst:
