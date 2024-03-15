61e480797e7e0ef073996addb32945420dc5d02b
rubinkim <kimyongha2011@gmail.com>
2024-03-15 18:02:03 +0900

    14696_딱지놀이.py

--- SW_IM/1244_스위치켜고끄기.py
+++ SW_IM/1244_스위치켜고끄기.py
@@ -11,4 +11,9 @@
 
 N = int(input())                        # 스위치 개수
 lst = list(map(int, input().split()))   # 스위치 리스트
-K = int(input())                        # 학생수  
+K = int(input())                        # 학생수
+k_lst = []
+for _ in range(K):
+    gender, switch_num = map(int, input().split())
+    k_lst.append((gender, switch_num))
+      
