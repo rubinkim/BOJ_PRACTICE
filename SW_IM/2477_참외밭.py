0cbac140bcaa68d6751198bf81ce8810d7b2c191
rubinkim <kimyongha2011@gmail.com>
2024-03-17 16:35:59 +0900

    2477_참외.py

--- SW_IM/2477_참외밭.py
+++ SW_IM/2477_참외밭.py
@@ -178,6 +178,8 @@ max_width = max([lst[i][1]  for i in range(6) if lst[i][0] in [1, 2]])
 total_plantable_area = 0
 max_area = max_height * max_width
 
+def calculate_area(lst):
+    global max_height, max_width
 
 print(calculate_area(lst))
                 
