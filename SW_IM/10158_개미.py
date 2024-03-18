7ac1f8587c1d801f87a3d47b58a7f42f03fd9add
rubinkim <kimyongha2011@gmail.com>
2024-03-18 16:55:11 +0900

    10158_개미.py

--- SW_IM/10158_개미.py
+++ SW_IM/10158_개미.py
@@ -16,3 +16,9 @@
 w, h = map(int, input().split())
 p, q = map(int, input().split())
 t = int(input())
+
+def convert_coord(w, h, p, q):   # 주어진 좌표를 원래대로 변경
+    return (h-q, p)
+
+def reverse_convert_coord(w, h, i, j):   # 원래 좌표를 문제에서 주어진 좌표로 변경
+    return (j, h - i)
