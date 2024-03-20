0ecefb8b43e5c97736e657f25dfe51b17dc4831d
rubinkim <kimyongha2011@gmail.com>
2024-03-20 15:43:58 +0900

    2491_sequence.py

--- SW_IM/2491_수열.py
+++ SW_IM/2491_수열.py
@@ -12,38 +12,42 @@
 (8   4   2)
 """
 
+import sys
+sys.setrecursionlimit(105000)
+input = sys.stdin.readline
 N = int(input())
 nums = list(map(int, input().split()))
-#print(f"N : {N},  nums : {nums}")
-
-#print()
-ans = []
+ans = [[0]]
+max_len = 1
 
 def dfs(n, lst_asc, lst_desc):      # n : nums라는 list의 각 원소들의 index
-    global ans
+    global ans, max_len
+    max_len = max([len(x) for x in ans])
     if n >= N:
-        ans.append(lst_asc)
-        ans.append(lst_desc)
+        if len(lst_asc) > max_len:
+            ans.append(lst_asc)
+        if len(lst_desc) > max_len:
+            ans.append(lst_desc)
         return
     elif n < N:
         if nums[n] > nums[n-1]:
-            ans.append(lst_desc)
+            if len(lst_desc) > max_len:
+                ans.append(lst_desc)
             lst_desc = [nums[n]]
             dfs(n+1, lst_asc+[nums[n]], lst_desc)
     
         elif nums[n] == nums[n-1]:
-            ans.append(lst_asc)
-            ans.append(lst_desc)
+            if len(lst_asc) > max_len:
+                ans.append(lst_asc)
+            if len(lst_desc) > max_len:
+                ans.append(lst_desc)
             dfs(n+1, lst_asc+[nums[n]], lst_desc+[nums[n]])
         
         elif nums[n] < nums[n-1]:
-            ans.append(lst_asc)
+            if len(lst_asc) > max_len:
+                ans.append(lst_asc)
             lst_asc = [nums[n]]
             dfs(n+1, lst_asc, lst_desc+[nums[n]])
         
 dfs(1, [nums[0]], [nums[0]])
-max_len = 0
-for x in ans:
-    if len(x) > max_len:
-        max_len = len(x)
 print(max_len)
