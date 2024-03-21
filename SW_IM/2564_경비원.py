b37512dea26fc04bcd3bf705059dc953636d819b
rubinkim <kimyongha2011@gmail.com>
2024-03-21 15:27:55 +0900

    2564_경비원.py

--- SW_IM/2564_경비원.py
+++ SW_IM/2564_경비원.py
@@ -14,7 +14,7 @@ w, h = map(int, input().split())
 N = int(input())
 lst = []
 
-# direction : 1(north), 2(south), 
+# direction : 1(north), 2(south), 3(west), 4(east)
 for _ in range(N+1):
     direction, distance = map(int, input().split())
     if 
