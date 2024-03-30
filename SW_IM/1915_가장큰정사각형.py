aeddabbebab62b9449aff5c940d7a0fbea7609b0
rubinkim <kimyongha2011@gmail.com>
2024-03-30 11:46:33 +0900

    1915_largest_square.py

--- SW_IM/1915_가장큰정사각형.py
+++ SW_IM/1915_가장큰정사각형.py
@@ -24,4 +24,9 @@ for i in range(1, n):
         for j in range(1, m):
             if arr[i][j] == 1:
                 dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
-                
+
+for i in range(n):
+    for j in range(m):
+        ans = max(ans, dp[i][j])
+
+print(ans**2)
