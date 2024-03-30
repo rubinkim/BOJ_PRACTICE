a6f11e346c06d9971e9691151961f95b33c88668
rubinkim <kimyongha2011@gmail.com>
2024-03-30 16:01:03 +0900

    more on 1915.py

--- SW_IM/1915_가장큰정사각형.py
+++ SW_IM/1915_가장큰정사각형.py
@@ -7,7 +7,7 @@
 0010
 
 (4)
-"""
+
 
 n, m = map(int, input().split())
 arr = [[] for _ in range(n)]
@@ -29,12 +29,22 @@ for j in range(m):
 for i in range(1, n):
     if arr[i][0] == 1:
         dp[i][0] = 1
-        for j in range(1, m):
-            if arr[i][j] == 1:
-                dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
+    for j in range(1, m):
+        if arr[i][j] == 1:
+            dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
 
 for i in range(n):
     for j in range(m):
         ans = max(ans, dp[i][j])
 
 print(ans**2)
+"""
+n, m = map(int, input().split())
+arr = [[] for _ in range(n)]
+for i in range(n):
+    row = list(map(int, input()))
+    for j in range(m):
+        arr[i].append(row[j])  
+        
+for row in arr:
+    print(row) 
