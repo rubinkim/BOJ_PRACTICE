fe104a2dd19a04567133642f5bb16109134ed27d
rubinkim <kimyongha2011@gmail.com>
2024-04-02 17:45:02 +0900

    1181_단어정렬.py

--- SW_IM/1193_분수찾기.py
+++ SW_IM/1193_분수찾기.py
@@ -6,27 +6,27 @@
 """
 dy = (1, -1, 0, 1)
 dx = (0, 1, 1, -1)
-y, x = 0, 0
+y, x = 1, 1
 
 n = int(input())
 step = 0
 dr = 0
 
 while step <= n:
-    if y == 0 and x == 0:
+    if y == 1 and x == 1:
         dr = 0
-    if y > 0 and y % 2 == 0 and x == 0:
-        dr = 0
-    if y % 2 == 1 and x == 0:
+    if y > 1 and y % 2 == 0 and x == 1:
         dr = 1
-    if y == 0 and x % 2 == 1:
+    if y == 0 and x > 1 and x % 2 == 0:
         dr = 2
-    if y == 0 and x > 0 and x % 2 == 0:
+    if y == 0 and x > 0 and x % 2 == 1:
         dr = 3
+    if y > 1 and y % 2 == 1 and x == 0:
+        dr = 0
     y = y + dy[dr]
     x = x + dx[dr]
     step += 1
 
     if step == n:
-        print(str(y+1) + '/' + str(x+1))
+        print(str(y) + '/' + str(x))
         break
