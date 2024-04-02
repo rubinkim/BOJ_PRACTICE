1bb3f2ab480a11b8f92be16ba9b6bd9ae1bda268
rubinkim <kimyongha2011@gmail.com>
2024-04-02 17:15:18 +0900

    1181_단어정렬.py

--- SW_IM/1181_단어정렬.py
+++ SW_IM/1181_단어정렬.py
@@ -35,3 +35,13 @@ for l in lst:
             print(w)
 """
 
+N = int(input())
+lst = []
+
+for _ in range(N):
+    lst.append(input())
+
+lst = list(set(lst))
+lst.sort(key=lambda x : (len(x),x))
+
+print("\n".join(lst))
