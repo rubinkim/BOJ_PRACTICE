fc6667b080396280147afc1883ea37525c667f66
rubinkim <kimyongha2011@gmail.com>
2024-04-03 14:08:28 +0900

    1193_분수찾기.py

--- SW_IM/1251_단어나누기.py
+++ SW_IM/1251_단어나누기.py
@@ -7,8 +7,9 @@ min_ord = 1e5
 
 for ch in word:
     min_ord = min(min_ord, ord(ch))
-#min_idx = word.index(ch(min_ch))
+min_idx = word.index(ch(min_ch))
 print(min_ord)
+print(min_idx)
 
 min_chr = chr(min_ord)
 print(min_chr)
