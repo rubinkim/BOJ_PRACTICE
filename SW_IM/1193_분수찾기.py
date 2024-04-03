e690b6dc20cc8a0f1c787d7320e23fd64afbf06f
rubinkim <kimyongha2011@gmail.com>
2024-04-03 12:57:59 +0900

    1193_분수찾기.py

--- SW_IM/1193_분수찾기.py
+++ SW_IM/1193_분수찾기.py
@@ -43,14 +43,16 @@ while sum < n:
     i += 1
 
 print(lst)
+if len(lst) == 1:
+    ans = (1, 1)
+else:
+    leftover = n - lst[-2]
+    if len(lst) % 2 == 1:
+        start = (len(lst), 1)
+        ans = (len(lst)-leftover+1, leftover)
+    elif len(lst) % 2 == 0:
+        start = (1, len(lst))
+        ans = (leftover, len(lst)-leftover+1)
 
-leftover = n - lst[-2]
-if len(lst) % 2 == 1:
-    start = (len(lst), 1)
-    ans = (len(lst)-leftover+1, leftover)
-elif len(lst) % 2 == 0:
-    start = (1, len(lst))
-    ans = (leftover, len(lst)-leftover+1)
-
-print(leftover)
+#print(leftover)
 print(ans)
