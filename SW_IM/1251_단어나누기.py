cc78a35a6ba16b6b7857a50e6f15514d6c61e2f6
rubinkim <kimyongha2011@gmail.com>
2024-04-03 14:12:51 +0900

    1193_분수찾기.py

--- SW_IM/1251_단어나누기.py
+++ SW_IM/1251_단어나누기.py
@@ -12,6 +12,10 @@ print(min_ord)
 print(min_idx)
 
 first_part = word[: min_idx+1]
-print(first_part)
+print(first_part[::-1])
 word = word[min_idx+1 :]
 print(word)
+
+min_ord = 1e5
+for ch in word:
+    min_ord = min()
