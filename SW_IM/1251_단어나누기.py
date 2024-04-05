3e88b8583d2613586747b836065e662663ef07ee
rubinkim <kimyongha2011@gmail.com>
2024-04-05 14:30:43 +0900

    1251_단어나누기.py

--- SW_IM/1251_단어나누기.py
+++ SW_IM/1251_단어나누기.py
@@ -6,7 +6,6 @@ arrested        (aerrdets)
 import sys
 input = sys.stdin.readline
 word = list(map(str, input().rstrip('\n')))
-print(word)
 
 res = []
 
@@ -22,24 +21,4 @@ for i in range(1, len(word)-1):
 
         res.append("".join(first_part + second_part + third_part))
         
-print(min(res))
-
-
-import sys
-
-word = list(map(str, sys.stdin.readline().rstrip("\n")))
-res = []
-
-for i in range(1, len(word) - 1):
-    for j in range(i + 1, len(word)):
-        first = word[:i] 
-        second = word[i:j] 
-        third = word[j:] 
-        
-        first.reverse()
-        second.reverse()
-        third.reverse()
-
-        res.append("".join(first + second + third))
-
 print(min(res))
