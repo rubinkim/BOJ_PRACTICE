fdef29a0515a75e9aca0ff17ef1917adaa448b1b
rubinkim <kimyongha2011@gmail.com>
2024-04-05 14:08:34 +0900

    1251_단어나누기.py

--- SW_IM/1251_단어나누기.py
+++ SW_IM/1251_단어나누기.py
@@ -3,24 +3,6 @@
 mobitel         (bometil)
 arrested        (aerrdets)
 """
-word = input()
-lst = []
-for i, ch in enumerate(word):
-    lst.append((i, ord(ch)))
-    
-#lst.sort(key=lambda x : (x[1], x[0]))    
-print(lst)
 
-min_idx = min([x[1] for x in lst])
-print(min_idx)
-
-min_idx_lst = []
-for i, ord_ch in lst:
-    if ord_ch == min_idx:
-        min_idx_lst.append(i)
-print(min_idx_lst)
-
-if len(min_idx_lst) >= 2 and min_idx_lst[-1] <= len(lst) - 2:
-    if 
 
     
