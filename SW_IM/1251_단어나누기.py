2b8386f5e1b3b7574f234f517dd5ba1fe5248422
rubinkim <kimyongha2011@gmail.com>
2024-04-05 14:20:25 +0900

    1251_단어나누기.py

--- SW_IM/1251_단어나누기.py
+++ SW_IM/1251_단어나누기.py
@@ -20,3 +20,6 @@ for i in range(1, len(word)):
         second_part.reverse()
         third_part.reverse()
 
+        res.append("".join(first_part + second_part + third_part))
+        
+print(min(res))
