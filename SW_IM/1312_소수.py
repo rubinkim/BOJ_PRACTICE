267b6742801cd7966d663c8d277159b239921f8d
rubinkim <kimyongha2011@gmail.com>
2024-04-08 13:25:02 +0900

    1312_소수.py

--- SW_IM/1312_소수.py
+++ SW_IM/1312_소수.py
@@ -4,8 +4,10 @@
 
 (2)
 """
-
+import math
 a, b, n = map(int, input().split())
 print(f"a : {a}, b : {b}, n : {n}")
 
 print(f"a/b : {a * 10**n / b}")
+ln_ans = math.log(a * 10 ** n / b)
+print(f"ln_ans : {ln_ans}")
