0287300bf6419e0768615ee7300d366f97212d7e
rubinkim <kimyongha2011@gmail.com>
2024-04-08 17:36:16 +0900

    1312_소수.py

--- SW_IM/1340_연도진행바.py
+++ SW_IM/1340_연도진행바.py
@@ -8,5 +8,10 @@ date = input()
 date = date.replace(',', ' ')
 date = date.replace(':', ' ')
 print(date)
+
+month_dict = {'January':1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
 m, d, y, hh, mm = date.split()
+m = 
 print(f"m : {m}, d : {d}, y : {y}, hh : {hh}, mm : {mm}")
+
+
