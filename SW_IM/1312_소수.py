ad18c400c8de81dcc023d5f14e20a51a95dae485
rubinkim <kimyongha2011@gmail.com>
2024-04-08 13:21:39 +0900

    1312_소수.py

--- SW_IM/1312_소수.py
+++ SW_IM/1312_소수.py
@@ -5,3 +5,10 @@
 (2)
 """
 
+a, b, n = map(int, input().split())
+#print(f"a : {a}, b : {b}, n : {n}")
+
+#print(f"a/b : {a * 10**n / b}")
+ans = str(a * (10 ** n) / b)
+ans = ans[:n+1]
+print(int(ans[-1]))
