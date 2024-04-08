2fd95c43cb6ad4d16d66a1bf1a3f99bc82f8c9a5
rubinkim <kimyongha2011@gmail.com>
2024-04-08 13:50:31 +0900

    1312_소수.py

--- SW_IM/1312_소수.py
+++ SW_IM/1312_소수.py
@@ -9,10 +9,17 @@ a, b, n = map(int, input().split())
 #print(f"a : {a}, b : {b}, n : {n}")
 
 #print(f"a/b : {a * 10**n / b}")
-ln_ans = math.log(a) + n *  math.log(10) - math.log(b)
+ln_ans = math.log(a) + n * math.log(10) - math.log(b)
 #print(f"ln_ans : {ln_ans}")
 ans = math.exp(ln_ans)
 ans = str(ans)[:n+1][-1]
 #print(f"ans : {ans}")
 print(ans)
 """
+import math
+a, b, n = map(int, input().split())
+ln_ans = math.log(a) + n * math.log(10) - math.lob(b)
+ln_ans_above = round(ln_ans, -1)
+print(f"ln_ans : {ln_ans},   ln_ans_above : {ln_ans_above}")
+ans = 1
+
