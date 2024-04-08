443f2c54ead1acd86c1be6edf2b44db776b66c76
rubinkim <kimyongha2011@gmail.com>
2024-04-08 13:59:38 +0900

    1312_소수.py

--- SW_IM/1312_소수.py
+++ SW_IM/1312_소수.py
@@ -20,7 +20,7 @@ import math
 a, b, n = map(int, input().split())
 ln_ans = math.log(a) + n * math.log(10) - math.log(b)
 ln_ans_above = math.floor(ln_ans)
-print(f"ln_ans : {ln_ans},   ln_ans_above : {ln_ans_above}")
+#print(f"ln_ans : {ln_ans},   ln_ans_above : {ln_ans_above}")
 ans = 1
 for i in range(ln_ans_above):
     ans = ans * math.exp(1)
