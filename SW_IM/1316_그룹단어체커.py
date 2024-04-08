aabc52d71a69620c5b22d6f3acde8da6b3e45548
rubinkim <kimyongha2011@gmail.com>
2024-04-08 17:21:13 +0900

    1312_소수.py

--- SW_IM/1316_그룹단어체커.py
+++ SW_IM/1316_그룹단어체커.py
@@ -18,3 +18,9 @@ ans = 0
 for _ in range(n):
     word = list(input())
     pure_word = [word[0]]
+    for i in range(1, len(word)):
+        if word[i] != word[i-1]:
+            pure_word.append(word[i])
+    if len(pure_word) == len(set(pure_word)):
+        ans += 1
+print(ans)
