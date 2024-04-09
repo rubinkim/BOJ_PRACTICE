f5c245787063f81619f89b088e68005feb9506ae
rubinkim <kimyongha2011@gmail.com>
2024-04-09 13:56:29 +0900

    1343_polyomino.py

--- SW_IM/1343_폴리오미노.py
+++ SW_IM/1343_폴리오미노.py
@@ -12,19 +12,5 @@ for i, ch in enumerate(board):
     if ch == 'X':
         cnt += 1
     elif ch == '.':
-        if cnt % 6 == 0:
-            poly.append('AAAABB' *  (cnt//6))
-            poly.append(ch)
-            cnt = 0
-        elif cnt % 4 == 0:
-            poly.append('AAAA' * (cnt//4))
-            poly.append(ch)
-            cnt = 0
-        elif cnt % 2 == 0:
-            poly.append('BB' * (cnt//2))
-            poly.append(ch)
-            cnt = 0
-        else:
-            print(-1)
-print("".join(poly))
+
             
