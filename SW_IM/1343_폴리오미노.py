bca5e36d9118f087ec6b741f7d73443f018bc58e
rubinkim <kimyongha2011@gmail.com>
2024-04-09 15:37:43 +0900

    more on 1343.py

--- SW_IM/1343_폴리오미노.py
+++ SW_IM/1343_폴리오미노.py
@@ -12,11 +12,8 @@ for i, ch in enumerate(board):
     if ch == 'X':
         cnt += 1
     elif ch == '.':
-        if cnt % 2 == 1:
-            print(-1)
-        else:
-            if cnt >= 6:
-                board[i-cnt : (i-cnt) * (cnt//6)] == 'AAAABB' * (cnt//6)
-                cnt -= 6 * (cnt//6)
+        poly.append(cnt)
+        cnt = 0
+print(poly)
             
             
