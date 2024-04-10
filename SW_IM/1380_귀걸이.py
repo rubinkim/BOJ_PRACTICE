37925937d70ee2584ec613ae9053439cf461064b
rubinkim <kimyongha2011@gmail.com>
2024-04-10 13:47:23 +0900

    1380_귀걸이.py

--- SW_IM/1380_귀걸이.py
+++ SW_IM/1380_귀걸이.py
@@ -43,7 +43,8 @@ while n > 0:
         num = int(num)
         if ch != name_dict[num][1]:
             del name_dict[num]
-    print(f"{scenario_num} {list(name_dict.values())[0][0]}")
+    #print(f"{scenario_num} {list(name_dict.values())[0][0]}")
+    print(f"{scenario_num} {name_dict.values()}")
     scenario_num += 1
 
             
