1890c4dff576a389e23d4e424ae1a1e086cd3205
rubinkim <kimyongha2011@gmail.com>
2024-04-11 17:41:26 +0900

    more on 1384.py

--- SW_IM/1384_메시지.py
+++ SW_IM/1384_메시지.py
@@ -49,3 +49,28 @@ if cnt == 0:
 print(cnt)
 """
 
+
+n = int(input())
+name_dict = {}
+for i in range(n):
+    row = list(input().split())
+    name_dict[(i, row[0])] = row[1:]
+    
+cnt = 0
+for idx, (k, v) in enumerate(name_dict.items()):
+    for i in range(len(v)):
+        if v[i] == 'N':
+            cnt += 1
+            num_backward = len(v) - 1 - i
+            if idx - num_backward < 0:
+                from_person = list(name_dict.keys())[len(name_dict.keys()) + idx - num_backward][1]    
+                to_person = k[1]
+                print(f"{from_person} was nasty about {to_person}")
+            else:
+                from_person = list(name_dict.keys())[idx - num_backward][1]
+                to_person = k[1]
+                print(f"{from_person} was nasty about {to_person}")
+if cnt == 0:
+    print("Nobody was nasty")
+print(cnt)
+
