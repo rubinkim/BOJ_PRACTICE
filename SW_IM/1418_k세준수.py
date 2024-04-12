72558fe2010f67aa540b17da72af5b4f8019a5e3
rubinkim <kimyongha2011@gmail.com>
2024-04-13 01:26:52 +0900

    1417_국회의원선거.py

--- SW_IM/1418_k세준수.py
+++ SW_IM/1418_k세준수.py
@@ -9,6 +9,7 @@ n = int(input())       # 주어지는 자연수 (1 <= n <= 1e5)
 k = int(input())       # k-세준수계산을 위한 k
 
 dp = [False] * (n+1)
+primes = []
 for num in range(2, n+1):
     prime = True
     for i in range(2, num):
@@ -16,3 +17,4 @@ for num in range(2, n+1):
             prime = False
     if prime:
         dp[num] = True
+        primes.append(num)
