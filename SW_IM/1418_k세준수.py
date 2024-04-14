9bdeb15e98c78e3f57df475ba3a9b9cfa68793e0
rubinkim <kimyongha2011@gmail.com>
2024-04-14 17:55:58 +0900

    more on 1418.py

--- SW_IM/1418_k세준수.py
+++ SW_IM/1418_k세준수.py
@@ -98,4 +98,6 @@ k_numbers = [1] * (n+1)
 for i in range(2, n+1):
     if arr[i] and i > k:
         for j in range(i, n+1, i):
+            k_numbers[j] = 0
+            
                            
