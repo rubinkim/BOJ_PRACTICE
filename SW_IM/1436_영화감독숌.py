f5a7aeb508bfdb4380966374d6985241f9e34d38
rubinkim <kimyongha2011@gmail.com>
2024-04-15 21:10:10 +0900

    1436_영화감독숌.py

--- SW_IM/1436_영화감독숌.py
+++ SW_IM/1436_영화감독숌.py
@@ -3,9 +3,11 @@
 187
 (66666)
 """
+import time
+start = time.time()
 n = int(input())
 cnt = 0
-for i in range(100001):
+for i in range(100000001):
     if '666' in str(i):
         cnt += 1
     if cnt == n:
