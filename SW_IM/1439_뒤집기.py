2af3990e032f3b59f34898a9eb3950311e5ff4ae
rubinkim <kimyongha2011@gmail.com>
2024-04-16 16:31:28 +0900

    1439_뒤집기.py

--- SW_IM/1439_뒤집기.py
+++ SW_IM/1439_뒤집기.py
@@ -25,6 +25,10 @@ while i <= len(s)-1:
     elif s[i-1] != s[i] and s[i] == '0':
         cnt_zero += 1
     i += 1
+if s[-2] != s[-1] and s[-1] == '1':
+    cnt_zero += 1
+if s[-2] != s[-1] and s[-1] == '0':
+    cnt_one += 1
       
     
 print(f"cnt_zero : {cnt_zero},   cnt_one : {cnt_one}")
