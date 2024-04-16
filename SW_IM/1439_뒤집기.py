7db37d94a03393717ec7df4fe0bbf05ebbeb0bc6
rubinkim <kimyongha2011@gmail.com>
2024-04-16 16:12:15 +0900

    1439_뒤집기.py

--- SW_IM/1439_뒤집기.py
+++ SW_IM/1439_뒤집기.py
@@ -26,7 +26,7 @@ while i <= len(s)-1:
         cnt_one += 1
     elif s[i-1] == '1' and s[i] == '0':
         cnt_zero += 1
-    elif s[i-1] == 1 and s[i] == 1:
+    elif s[i-1] == '1' and s[i] == '1':
         continue
     i += 1    
     
