830b47910884433a1749b49e6596cfab2aa9664f
rubinkim <kimyongha2011@gmail.com>
2024-04-16 16:23:06 +0900

    1439_뒤집기.py

--- SW_IM/1439_뒤집기.py
+++ SW_IM/1439_뒤집기.py
@@ -20,9 +20,9 @@ s = list(input())
 cnt_zero, cnt_one = 0, 0    # cnt_zero : number of flipping to make every element zero,  cnt_one : vice versa
 i = 1
 while i <= len(s)-1:
-    if s[i-1] == '0' and s[i] == '1':
+    if s[i-1] != s[i] and s[i] == '1':
         cnt_one += 1        
-    elif s[i-1] == '1' and s[i] == '0':
+    elif s[i-1] != s[i] and s[i] == '0':
         cnt_zero += 1
     i += 1
       
