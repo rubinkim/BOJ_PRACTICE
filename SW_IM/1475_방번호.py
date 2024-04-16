d2eb36325a6510344c154aa5496fd26a6846f3a6
rubinkim <kimyongha2011@gmail.com>
2024-04-16 18:00:15 +0900

    1439_뒤집기.py

--- SW_IM/1475_방번호.py
+++ SW_IM/1475_방번호.py
@@ -11,4 +11,40 @@
 
 888888
 (6)
+
+126549666
+(3)
 """
+
+from collections import Counter
+lst = list(map(int, input()))     # 126549666
+print(lst)
+
+num_dict = Counter(lst)
+print(num_dict)
+
+for k, v in num_dict.items():
+    if num_dict[6] and num_dict[9]:
+        if (num_dict[6] + num_dict[9]) % 2 == 0:
+            num_dict[6] += num_dict[9]
+            num_dict[6] = num_dict[6] // 2
+            num_dict[9] = 0
+        elif (num_dict[6] + num_dict[9]) % 2 == 1:
+            num_dict[6] += num_dict[9]
+            num_dict[6] = num_dict[6] // 2 + 1
+            num_dict[9] = 0
+            
+    elif num_dict[6] and not num_dict[9]:
+        if num_dict[6] % 2 == 0:
+            num_dict[6] = num_dict[6] // 2
+        elif num_dict[6] % 2 == 1:
+            num_dict[6] = num_dict[6] // 2 + 1  
+    
+    elif not num_dict[6] and num_dict[9]:
+        if num_dict[9] % 2 == 0:
+            num_dict[9] = num_dict[9] // 2
+        elif num_dict[9] % 2 == 1:
+            num_dict[9] = num_dict[9] // 2 + 1
+            
+print(num_dict)
+print(max(num_dict.values()))
