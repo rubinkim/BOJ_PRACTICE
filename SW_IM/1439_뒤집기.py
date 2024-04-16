c5412546de99cbf857057b1ddf799c91e25b5122
rubinkim <kimyongha2011@gmail.com>
2024-04-16 16:06:58 +0900

    1439_뒤집기.py

--- SW_IM/1439_뒤집기.py
+++ SW_IM/1439_뒤집기.py
@@ -17,9 +17,13 @@
 """
 s = list(input())
 
-cnt_zero, cnt_ont = 0, 0    # cnt_zero : number of flipping to make every element zero,  cnt_one : vice versa
+cnt_zero, cnt_one = 0, 0    # cnt_zero : number of flipping to make every element zero,  cnt_one : vice versa
 i = 1
-
+while i <= len(s)-1:
+    if s[i-1] == 0 and s[i] == 0:
+        continue
+    elif s[i-1] == 0 and s[i] == 1:
+        cnt_one += 1
 
 
         
