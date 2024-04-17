0445b9f01467fd630d9c69f8d769fd851f430221
rubinkim <kimyongha2011@gmail.com>
2024-04-17 13:31:27 +0900

    1476.py

--- SW_IM/1531_투명.py
+++ SW_IM/1531_투명.py
@@ -8,20 +8,5 @@
 (500)
 """
 
-n, m = map(int, input().split())
-board = [[0] * 100 for _ in range(100)]
 
-for _ in range(n):
-    xd, yd, xu, yu = map(int, input().split())
-    yd, yu = 100-yd, 100-yu
-    for i in range(yu, yd+1):
-        for j in range(xd, xu+1):
-            board[i][j] += 1
-            
-ans = 0
-for i in range(100):
-    for j in range(100):
-        if board[i][j] > m:
-            ans += 1
-print(ans)
 
