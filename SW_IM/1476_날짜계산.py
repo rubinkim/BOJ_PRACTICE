5cbd887614a6d9cacfe11bd1681d697f716e4ffd
rubinkim <kimyongha2011@gmail.com>
2024-04-17 12:40:45 +0900

    1476.py

--- SW_IM/1476_날짜계산.py
+++ SW_IM/1476_날짜계산.py
@@ -17,7 +17,6 @@ e, s, m = map(int, input().split())
 year = 1
 
 while True:
-    if year % 15 == e and year % 28 == s and year % 19 == m:
+    if ((year - 1) * 15 + e) % 15 == e and ((year - 1) * 28 + s) % 28 == s and ((year - 1) * 19 + m) % 19 == m:
         print(year)
-        break
-    year += 1
+        break    
