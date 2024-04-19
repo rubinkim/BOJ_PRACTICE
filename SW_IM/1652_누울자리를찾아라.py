e7376c64ee11ef8417c7c066c7985041a134bfc7
rubinkim <kimyongha2011@gmail.com>
2024-04-19 14:27:18 +0900

    1543.문서검색.py

--- SW_IM/1652_누울자리를찾아라.py
+++ SW_IM/1652_누울자리를찾아라.py
@@ -14,8 +14,9 @@ arr = [[0] * n for _ in range(n)]
 for i in range(len(arr)):
     row = input().split()
     for j in range(len(row)):
-        
-    
+        arr[i][j] = row[j]
+for row in arr:
+    print(row)    
     
 for i in range(n):
     row = input().split()
