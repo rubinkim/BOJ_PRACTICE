b52c722c70aae64cc380a741c3d82bac81549f24
rubinkim <kimyongha2011@gmail.com>
2024-04-19 13:01:25 +0900

    1543.문서검색.py

--- SW_IM/1543_문서검색.py
+++ SW_IM/1543_문서검색.py
@@ -26,7 +26,14 @@ word = input()
 
 cnt = 0
 i = 0
-while len(lst) >= len(word):
+while len(lst) >= len(word) or i <:
     if lst[:len(word)] == word:
         cnt += 1
+        i += len(word)
         lst = lst[len(word):]
+    elif lst[:len(word)] != word:
+        i += 1
+        lst = lst[i:]
+    if len(lst) == len(word) and lst != word:
+        break
+print(cnt)
