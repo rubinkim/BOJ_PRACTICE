dfec65c78b68c1ca382f6f02cfaa09aff2fc7029
rubinkim <kimyongha2011@gmail.com>
2024-04-19 14:22:50 +0900

    1543.문서검색.py

--- SW_IM/1652_누울자리를찾아라.py
+++ SW_IM/1652_누울자리를찾아라.py
@@ -17,4 +17,9 @@ for i in range(n):
     for j in range(n):
         if arr[i][j] == '.':
             arr[i][j] = 0
+        elif arr[i][j] == 'X':
+            arr[i][j] = 1
+            
+for row in arr:
+    print(row)
         
