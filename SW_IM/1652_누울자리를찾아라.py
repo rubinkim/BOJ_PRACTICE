b57277fa93d1482ae46f080ea7874715a3747013
rubinkim <kimyongha2011@gmail.com>
2024-04-19 15:11:26 +0900

    1543.문서검색.py

--- SW_IM/1652_누울자리를찾아라.py
+++ SW_IM/1652_누울자리를찾아라.py
@@ -44,5 +44,13 @@ for i in range(len(arr)):
     if 1 <= i <= i-2 and sum(arr[i]) == 0 and (sum(arr[i-1]) >= 1 or sum(arr[i+1]) >= 1):
         col_cnt += 1
         
-        
+for i in range(len(arr_tr)):
+    if any([arr_tr[i][j:j+2] == 0 for j in range(len(arr_tr[i])-1)]) and sum(arr_tr[i]) >= 1:
+        row_cnt += 1
+    if i == 0 and sum(arr_tr[i]) == 0 and sum(arr_tr[i+1]) >= 1:
+        row_cnt += 1
+    if i == n-1 and sum(arr_tr[i]) == 0 and sum(arr_tr[i-1]) >= 1:
+        row_cnt += 1
+    if 1 <= i <= i-2 and sum(arr_tr[i]) == 0 and (sum(arr_tr[i-1]) >= 1 or sum(arr_tr[i+1]) >= 1):
+        row_cnt += 1    
         
