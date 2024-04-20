d0e2756eaf0316da79bafcaf4623a1472c513dcb
rubinkim <kimyongha2011@gmail.com>
2024-04-21 06:58:13 +0900

    more on 1652.py

--- SW_IM/1676_팩토리얼0의개수.py
+++ SW_IM/1676_팩토리얼0의개수.py
@@ -8,3 +8,26 @@
 
 (0)
 """
+
+n = int(input())
+cache = [0] * (n+1)
+
+def cal_factorial(x):
+    if x <= 1:
+        return 1
+    else:
+        cache[x] = x * cal_factorial(x-1)
+        return cache[x]
+
+ans = 0
+my_fac = cal_factorial(n)
+print(my_fac)
+
+if my_fac % 10 != 0:
+    print(ans)
+else:
+    i = 0
+    while my_fac % (10 ** i) == 0:
+        ans += 1
+        i += 1
+    print(ans-1)
