78874c45fd9d0586fc6ce971fdb0aaafe9e3c9f2
rubinkim <kimyongha2011@gmail.com>
2024-04-21 20:15:01 +0900

    more on 1769.py

--- SW_IM/1789_수들의합.py
+++ SW_IM/1789_수들의합.py
@@ -4,13 +4,13 @@
 
 19
 """
-x = list(range(20))
-cum_x = [0] * 100
+x = list(range(100001))
+cum_x = [0] * 100001
 cum_x[0] = 0
 cum_x[1] = 1
 
-for i in range(2, 100):
+for i in range(2, 100001):
     cum_x[i] = cum_x[i-1] + i
     
-print(x)
-print(cum_x[:20])
+print(x[100000])
+print(cum_x[100000])
