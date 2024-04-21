a2d64ce18015ab5438237f35fccf2d57d238ce3f
rubinkim <kimyongha2011@gmail.com>
2024-04-21 15:39:12 +0900

    more on 1769.py

--- SW_IM/1769_3의배수.py
+++ SW_IM/1769_3의배수.py
@@ -5,3 +5,28 @@
 3
 NO
 """
+import time
+start = time.time()
+
+import sys
+input = sys.stdin.readline
+x = int(input())
+x_added = 0
+i = 0
+while True:
+    if x < 1 and x_added >= 10:
+        x = x_added
+        x_added = 0
+        i += 1
+    if x < 1 and x_added < 10:
+        i += 1
+        break
+        
+    remainder = x % 10  
+    x //= 10
+    x_added += remainder
+print(i)
+print("YES" if x_added % 3 == 0 else "NO")
+
+end = time.time()
+print(f"time elapsed : {end - start}")
