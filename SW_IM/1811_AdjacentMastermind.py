5b44f106a4c4521723fd3e7c6c78c121785cf1aa
rubinkim <kimyongha2011@gmail.com>
2024-04-22 19:11:26 +0900

    1811_AdjacentMastermind.py

--- SW_IM/1811_AdjacentMastermind.py
+++ SW_IM/1811_AdjacentMastermind.py
@@ -9,5 +9,11 @@ ABCD ABCD
 ABABAA BCBCAA
 #
 
-
+AAAA: 1 black, 0 grey, 0 white
+ACEB: 1 black, 1 grey, 1 white
+EFBB: 0 black, 1 grey, 0 white
+ACBC: 1 black, 2 grey, 0 white
+BEAA: 0 black, 1 grey, 1 white
+ABCD: 4 black, 0 grey, 0 white
+BCBCAA: 2 black, 2 grey, 0 white
 """
