b1d0eff5cd2d4fc26bc1de3fac0d21516e425faa
rubinkim <kimyongha2011@gmail.com>
2024-04-22 19:41:30 +0900

    1811_AdjacentMastermind.py

--- SW_IM/1811_AdjacentMastermind.py
+++ SW_IM/1811_AdjacentMastermind.py
@@ -35,7 +35,7 @@ while True:
         elif 0 < i < len(target)-1 and (target[i] == guess[i-1] or target[i] == guess[i+1]):
             grey_cnt += 1
         elif i == len(target)-1 and target[i] == guess[i-1]:
-            g
+            grey_cnt += 1
         
     print(f"{guess}: {black_cnt} black")
         
