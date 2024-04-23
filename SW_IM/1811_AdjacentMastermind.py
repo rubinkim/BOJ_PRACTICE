1d2ce2fe57aa082b41545b72db4df8aa301432da
rubinkim <kimyongha2011@gmail.com>
2024-04-23 14:38:49 +0900

    more on 1811.py

--- SW_IM/1811_AdjacentMastermind.py
+++ SW_IM/1811_AdjacentMastermind.py
@@ -119,5 +119,6 @@ while True:
             if 1 < i < len(guess)-2 and (j <= i-2 or j >= i+2) and guess[i] == target[j] and not guess_chk[i] and not target_chk[j]:
                 white_cnt += 1
                 guess_chk[i] = True
+                target_chk[j] = True
 
 
