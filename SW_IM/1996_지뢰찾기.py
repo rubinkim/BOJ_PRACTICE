f5a1e169dc2b1ab55dc7401245f08db979b6babb
rubinkim <kimyongha2011@gmail.com>
2024-04-24 21:13:19 +0900

    more on 1996.py

--- SW_IM/1996_지뢰찾기.py
+++ SW_IM/1996_지뢰찾기.py
@@ -20,9 +20,14 @@ for i in range(n):
     arr.append(input())
     
 arr = ["." * (n+2)] + ["." + arr[i] + "." for i in range(n)] + ["." * (n+2)]
+str_nums = list(map(str, range(1, 10)))
 
 for i in range(len(arr)):
     for j in range(len(arr[i])):
+        if arr[i][j] == ".":
+            arr[i][j] = 0
+        elif arr[i][j] in str_nums:
+            arr[i][j] = int(arr[i][j])
     
 for row in arr:
     print(row)
