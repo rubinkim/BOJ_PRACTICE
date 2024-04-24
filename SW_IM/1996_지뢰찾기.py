7d9cefb7e86d91bf4269a073b8ec3acb66cfa6a8
rubinkim <kimyongha2011@gmail.com>
2024-04-24 21:21:47 +0900

    more on 1996.py

--- SW_IM/1996_지뢰찾기.py
+++ SW_IM/1996_지뢰찾기.py
@@ -37,3 +37,5 @@ for row in arr:
 mines = [[0] * (n+2) for _ in range(n+2)]
 for i in range(1, n+1):
     for j in range(1, n+1):
+        if arr[i][j] == 0:
+            mines[i][j] = sum([sum([arr[x][y] for y in range(j-1, j+2)] for x in range(i-1, i+2))])
