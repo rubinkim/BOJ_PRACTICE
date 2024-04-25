1cb51cbc92c6cade0acfd445d16af94054f53b16
rubinkim <kimyongha2011@gmail.com>
2024-04-25 18:28:43 +0900

    2018.py

--- SW_IM/2057_팩토리얼분해.py
+++ SW_IM/2057_팩토리얼분해.py
@@ -4,3 +4,22 @@
 
 NO
 """
+
+left, right = 0, 0
+ans, acc = 0, 0
+
+n = int(input())
+
+while left <= right:
+    if acc == n:
+        print("YES")
+        break
+    elif acc > n:
+        right += 1
+        acc += dp[right]
+    elif acc < n:
+        acc -= dp[left]
+        left += 1
+        
+if left > right:
+    print("NO")
