b8827f80c9681d3a4d09af5d989990f4375ee880
rubinkim <kimyongha2011@gmail.com>
2024-04-28 07:47:34 +0900

    more on 2057.py

--- SW_IM/2097_조약돌.py
+++ SW_IM/2097_조약돌.py
@@ -6,16 +6,4 @@
 14
 (12)
 """
-import math
-max_i = round((math.log(5) + 8 * math.log(10)) / math.log(2)) + 1
-#print(max_i)
-
-n = int(input())
-for i in range(1, max_i):
-    if i == 1:
-        if 1 <= n <= (i+1) ** 2:
-            print(4 * i)
-    elif i > 1:
-        if i ** 2 + 1 <= n <= (i+1) ** 2:
-            print(4 * i)  #
-        
+n = int(input())    # 조약돌들의 갯수
