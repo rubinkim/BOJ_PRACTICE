4786d161f55bc00183a48e6f61e55e5d512bc3cc
rubinkim <kimyongha2011@gmail.com>
2024-04-28 07:55:35 +0900

    more on 2057.py

--- SW_IM/2097_조약돌.py
+++ SW_IM/2097_조약돌.py
@@ -8,7 +8,7 @@
 """
 n = int(input())    # 조약돌들의 갯수
 max_side_plus_one = 0
-for i in range(int(n**0.5)):
+for i in range(int((n-1)**0.5)+1):
     if i ** 2 <= n:
         max_side_plus_one = max(max_side_plus_one, i)
 print(max_side_plus_one)
