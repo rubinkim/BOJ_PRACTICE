e55672911cbf11b6c06fca260cb3ec0520efe847
rubinkim <kimyongha2011@gmail.com>
2024-04-28 18:55:49 +0900

    2161.py

--- SW_IM/2238_경매.py
+++ SW_IM/2238_경매.py
@@ -10,3 +10,10 @@ CD 7
 """
 u, n = map(int, input().split())
 name_lst, price_lst, final_lst = [], [], []
+for _ in range(n):
+    name, price = map(input().split())
+    name_lst.append(name)
+    price_lst.append(price)
+
+print(name_lst)
+print(price_lst)
