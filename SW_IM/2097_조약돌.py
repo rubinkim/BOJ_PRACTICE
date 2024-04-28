880eebe9de1baca0eeec78643674d0657379b6f9
rubinkim <kimyongha2011@gmail.com>
2024-04-28 16:06:01 +0900

    2161.py

--- SW_IM/2097_조약돌.py
+++ SW_IM/2097_조약돌.py
@@ -11,7 +11,7 @@ max_side_plus_one = 0
 for i in range(int((n-1)**0.5)+1):
     if i ** 2 <= n:
         max_side_plus_one = max(max_side_plus_one, i)
-print(max_side_plus_one)
+#print(max_side_plus_one)
 
 one_side_length = max_side_plus_one - 1
 
