eaeda9c3f56bdf09e166252d5d6b679be13bfb79
rubinkim <kimyongha2011@gmail.com>
2024-04-29 09:53:31 +0900

    2303_숫자게임.py

--- SW_IM/2508_사탕박사고창영.py
+++ SW_IM/2508_사탕박사고창영.py
@@ -30,3 +30,34 @@ for _ in range(tc):
                     cnt += 1
     print(cnt)
 """
+tc = int(input())
+
+for _ in range(tc):
+    input()    
+    r, c = map(int, input().split())
+    box = [[] for _ in range(r)]
+    for i in range(r):
+        box[i] = input()
+    
+    cnt = 0
+    if r <= 2 and c <= 2:
+        cnt = 0
+    if r == 2 and c >= 3:
+        for i in range(r):
+            for j in range(1, c-1):
+                if ord(box[0][j-1]) == 62 and ord(box[0][j] == 111 and ord(box[0][j+1] == 60:
+                    cnt = 1
+    if r >= 3 and c == 2:                                                                           
+        for i in range(1, r-1):
+            for j in range(c):                                                                           
+                if ord(box[i-1][0]) == 118 and ord(box[i][0] == 111 and ord(box[i+1][0] == 94:
+                    cnt = 1
+    if r >= 3 and c >= 3:
+        for i in range(1, r-1):
+            for j in range(1, c-1):
+                if ord(box[i][j]) == 111:
+                    if (1 <= i <= r-1) and (ord(box[i-1][j]) == 118 and ord(box[i+1][j]) == 94):
+                        cnt += 1
+                    if (1 <= j <= c-1) and (ord(box[i][j-1]) == 62 and ord(box[i][j+1]) == 60):
+                        cnt += 1
+    print(cnt)
