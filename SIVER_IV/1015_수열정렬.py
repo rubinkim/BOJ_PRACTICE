dbeb8d52d38befad19ef1a17ab209f0069e73ee3
rubinkim <kimyongha2011@gmail.com>
2024-05-01 12:32:49 +0900

    creating SIVER_IV

--- SIVER_IV/1015_수열정렬.py
+++ SIVER_IV/1015_수열정렬.py
@@ -23,21 +23,19 @@ print(f"n : {n},   a : {a}")
 
 p = []
 
-a_sort = []
+a_tuple = []
 for i in range(len(a)):
-    a_sort.append((a[i], i))
+    a_tuple.append((a[i], i))
     
-print(a_sort)
-a_sort.sort()
+print(a_tuple)
+a_sort = sorted(a_tuple)
 print(a_sort)
 
 lst = []
 chk = [False] * n
+
 for i in range(len(a)):
     for j in range(len(a_sort)):
-        if a[i] == a_sort[j][0] and not chk[j]:
-            chk[j] = True
-            lst.append(a_sort.index(a_sort[j]))
-
+        if a[i] == a_sort[j][0] and i == a_sort[j][1]:
+            lst.append(j)
 print(lst)
-
