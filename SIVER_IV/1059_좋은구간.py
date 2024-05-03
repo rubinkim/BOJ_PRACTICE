b0cd997980f8e8b9eaa7481eedb554c5b5abc637
rubinkim <kimyongha2011@gmail.com>
2024-05-03 21:18:28 +0900

    1049_기타줄.py

--- SIVER_IV/1059_좋은구간.py
+++ SIVER_IV/1059_좋은구간.py
@@ -31,3 +31,10 @@ print(f"l : {l}, s : {s}, n : {n}")
 cnt = 0
 for i in range(l-1):
     if s[i] < n < s[i+1]:
+        for j in range(s[i]+1, n+1):
+            cnt += s[i+1]-1-j
+        break
+    if s[i] == n:
+        cnt = 0
+        break
+print(cnt)
