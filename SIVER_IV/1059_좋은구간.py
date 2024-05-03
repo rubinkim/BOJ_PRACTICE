a1bfc0933ba193b3dc7640a7a90e41a7b1bf33ae
rubinkim <kimyongha2011@gmail.com>
2024-05-04 07:46:35 +0900

    more on 1059.py

--- SIVER_IV/1059_좋은구간.py
+++ SIVER_IV/1059_좋은구간.py
@@ -33,7 +33,7 @@ for i in range(l-1):
     if s[i] < n < s[i+1]:
         for j in range(s[i]+1, n+1):
             for k in range(n, s[i+1]):
-                cnt += s[i+1]-n
+                cnt += 1
         break            
 
     if s[i] == n or s[i+1] == n:
