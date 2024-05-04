0531d0fd33b68a7e9af485808d2abfdf026f2f56
rubinkim <kimyongha2011@gmail.com>
2024-05-04 15:13:43 +0900

    more on 1064.py

--- SIVER_IV/1065_한수.py
+++ SIVER_IV/1065_한수.py
@@ -20,4 +20,10 @@ cnt = 0
 for x in range(0, n+1):
     if 1 <= x <= 99:
         cnt += 1
+    elif x >= 100:
+        x = str(x)
+        if (int(x[0]) - int(x[1])) == (int(x[1]) - (int(x[2]))):
+            cnt += 1
+        
+        
 print(cnt)
