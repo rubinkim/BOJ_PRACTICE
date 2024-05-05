1d3ea1651865937eac380c3baea00ceedccb20d7
rubinkim <kimyongha2011@gmail.com>
2024-05-05 15:36:53 +0900

    more on 1120.py

--- SIVER_IV/1120_문자열.py
+++ SIVER_IV/1120_문자열.py
@@ -22,7 +22,12 @@ print(f"a : {a},  b : {b}")
 diff = len(b) - len(a)
 print(f"diff : {diff}")
 
+cnt = 0
+if diff == 0:
+    
+
 for i in range(diff):
     new_a = b[:i] + a + b[:diff-i]
     print(f"i : {i},  new_a : {new_a},  b : {b}")
+    
     
