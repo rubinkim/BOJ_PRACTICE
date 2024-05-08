6a115c78104052cf141e02a5f39e60820265e7b3
rubinkim <kimyongha2011@gmail.com>
2024-05-08 14:44:34 +0900

    1235_학생번호.py

--- SIVER_IV/1246_온라인판매.py
+++ SIVER_IV/1246_온라인판매.py
@@ -17,11 +17,13 @@ print(f"bids : {bids}")
 
 max_bid, min_bid = max(bids), min(bids)
 lst = []
-for pr in range(max_bid, min_bid-1):
+for pr in range(max_bid, min_bid-1, -1):
     revenue = 0
     for i in range(len(bids)):
         if bids[i] >= pr:
             revenue += pr
     lst.append((revenue, pr))
     
-print(lst)
+new_lst = sorted(lst, key=lambda x : x[0], reverse=True)
+    
+print(new_lst)
