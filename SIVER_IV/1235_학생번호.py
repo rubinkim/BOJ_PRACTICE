603fb48c05009bc6ee9638c0d926e46ca41374c3
rubinkim <kimyongha2011@gmail.com>
2024-05-08 13:54:26 +0900

    1235_학생번호.py

--- SIVER_IV/1235_학생번호.py
+++ SIVER_IV/1235_학생번호.py
@@ -11,7 +11,7 @@ n = int(input())
 student_nums = []
 for _ in range(n):
     student_nums.append(input())
-print(student_nums)
+#print(student_nums)
 
 num_len = len(student_nums[0])
 print(num_len)
