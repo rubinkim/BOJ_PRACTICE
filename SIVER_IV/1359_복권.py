b6b8d5e082123ecab84415edef935e0b4b53af9a
rubinkim <kimyongha2011@gmail.com>
2024-05-15 11:17:54 +0900

    1359_lottery.py

--- SIVER_IV/1359_복권.py
+++ SIVER_IV/1359_복권.py
@@ -13,3 +13,25 @@
 (0.7571428571428571)
 """
 
+# 1359_lottery
+
+from itertools import combinations
+n, m, k = map(int, input().split())
+combis = []
+
+for combi in combinations(range(1, n+1), m):
+    combis.append(combi)
+
+#print(len(combis))
+
+#for comb in combis:
+    #print(comb)
+    
+cnt = 0
+for x in combis:
+    for y in combis:
+        if len(set(x).intersection(set(y))) >= k:
+             cnt += 1
+#print(cnt)
+
+print(cnt / len(combis) ** 2)
