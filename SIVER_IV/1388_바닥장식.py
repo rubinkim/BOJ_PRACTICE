f484ea3ff56f58f45b0dfb0fe0e8063e9e07cb16
rubinkim <kimyongha2011@gmail.com>
2024-05-16 20:46:17 +0900

    1388_바닥장식.py

--- SIVER_IV/1388_바닥장식.py
+++ SIVER_IV/1388_바닥장식.py
@@ -53,8 +53,8 @@ arr = []
 for _ in range(n):
     arr.append(input())
  
-for row in arr:
-    print(row)
+#for row in arr:
+    #print(row)
     
 arr_transpose = list(map(list, zip(*arr)))
 for row in arr_transpose:
