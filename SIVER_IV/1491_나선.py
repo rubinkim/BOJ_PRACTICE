95eab1c89388ef09a3c2d7303cc1768897ba7fab
rubinkim <kimyongha2011@gmail.com>
2024-05-19 06:49:46 +0900

    more on 1491.py

--- SIVER_IV/1491_나선.py
+++ SIVER_IV/1491_나선.py
@@ -18,9 +18,6 @@
 
 n, m = map(int, input().split())    # n : number of columns,  m : number of rows
 arr = [[0] * n for _ in range(m)]
-dy = [0, -1, 0, 1]
-dx = [1, 0, -1, 0]
-current = 0
 
 def dfs(y, x):
     dy = [0, -1, 0, 1]
