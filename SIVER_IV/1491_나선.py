70a251d1585e66394ff9c3ecddf92665c167664e
rubinkim <kimyongha2011@gmail.com>
2024-05-19 06:25:56 +0900

    more on 1491.py

--- SIVER_IV/1491_나선.py
+++ SIVER_IV/1491_나선.py
@@ -18,6 +18,8 @@
 
 n, m = map(int, input().split())    # n : number of columns,  m : number of rows
 arr = [[0] * n for _ in range(m)]
+dy = [0, -1, 0, 1]
+dx = [1, 0, -1, 0]
 
 for i in range(m):
     for j in range(n):
