5e7932e35cdb8f6791539eaa493aeabbbcd70707
rubinkim <kimyongha2011@gmail.com>
2024-05-19 18:06:39 +0900

    1544_cycleWords.py

--- SIVER_IV/1491_나선.py
+++ SIVER_IV/1491_나선.py
@@ -24,15 +24,17 @@ current = 0
 
 dy, dx = [0, -1, 0, 1], [1, 0, -1, 0]
 y, x = m-1, 0
+ans = 1
 
 while True:
-    if sum([sum([arr[i][j] for j in range(n)]) for i in range(m)]) == n * m:
+    if ans == n * m:
         print(x, m-1-y)
         break
     
     arr[y][x] = 1
     ny, nx = y + dy[current], x + dx[current]
     if 0 <= ny <= m-1 and 0 <= nx <= n-1 and arr[ny][nx] == 0:
+        ans += 1
         y, x = ny, nx
     else:
         current = (current+1) % 4
