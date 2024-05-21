d5eb36a1ceb2074c8f714d1ba575d22595f4a3bc
rubinkim <kimyongha2011@gmail.com>
2024-05-21 20:52:01 +0900

    1590_캠프가는영식.py

--- SIVER_IV/1620_나는야포켓몬마스터이다솜.py
+++ SIVER_IV/1620_나는야포켓몬마스터이다솜.py
@@ -49,29 +49,29 @@ for i in range(n):
 lst_by_name = sorted(lst, key=lambda x : x[1])
 
 for _ in range(m):
-    lo, hi = 0, len(new_lst)
+    lo, hi = 0, len(lst)
     mid = (lo + hi) // 2
     
     x = input()
-    if int(x) in list(range(1, n+1)):
+    if x in list(map(str, range(1, n+1))):
         while lo <= hi:
-            if int(x) == new_lst[mid-1][0]:
-                print(new_lst[mid-1][1])
+            if int(x) == lst[mid-1][0]:
+                print(lst[mid-1][1])
                 break
-            elif int(x) > new_lst[mid-1][0]:
+            elif int(x) > lst[mid-1][0]:
                 lo = mid + 1
-            elif int(x) < new_lst[mid-1][0]:
+            elif int(x) < lst[mid-1][0]:
                 hi = mid - 1
             mid = (lo + hi) // 2
                 
     else:
         while lo <= hi:
-            if x == new_lst[mid-1][1]:
-                print(new_lst[mid-1][0])
+            if x == lst_by_name[mid-1][1]:
+                print(lst_by_name[mid-1][0])
                 break
-            elif x > new_lst[mid-1][1]:
+            elif x > lst_by_name[mid-1][1]:
                 lo = mid + 1
-            elif x < new_lst[mid-1][1]:
+            elif x < lst_by_name[mid-1][1]:
                 hi = mid - 1
             mid = (lo + hi) // 2
     
