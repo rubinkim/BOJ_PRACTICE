3bcff6c9f30232bd4747a552804a2bdf00ab2b42
rubinkim <kimyongha2011@gmail.com>
2024-05-21 22:15:49 +0900

    more on 1620.py

--- SIVER_IV/1620_나는야포켓몬마스터이다솜.py
+++ SIVER_IV/1620_나는야포켓몬마스터이다솜.py
@@ -40,18 +40,21 @@ Venusaur
 14)
 """
 n, m = map(int, input().split())
-lst = []
+lst_num_name = []
+lst_name = []
+
 
 for i in range(n):
     x = input()
-    lst.append((i+1, x))
+    lst_num_name.append((i+1, x))
+    lst_name.append(x)
     
 for _ in range(m):
     x = input()
     if x in list(map(str, range(1, n+1))):
-        print(lst[int(x)-1][1])        
+        print(lst_num_name[int(x)-1][1])        
     else:
-        print(lst.index(x) + 1)
+        print(lst_name.index(x) + 1)
         
                 
     
