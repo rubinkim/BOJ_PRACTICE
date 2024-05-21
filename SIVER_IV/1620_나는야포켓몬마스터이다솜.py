1a6498bd342fda054bc001b7b34d539ec6a88280
rubinkim <kimyongha2011@gmail.com>
2024-05-21 22:03:47 +0900

    more on 1620.py

--- SIVER_IV/1620_나는야포켓몬마스터이다솜.py
+++ SIVER_IV/1620_나는야포켓몬마스터이다솜.py
@@ -46,35 +46,7 @@ for i in range(n):
     x = input()
     lst.append((i+1, x))
     
-lst_by_name = sorted(lst, key=lambda x : x[1])
 
-for _ in range(m):
-    lo, hi = 0, len(lst)
-    mid = (lo + hi) // 2
-    
-    x = input()
-    if x in list(map(str, range(1, n+1))):
-        while lo <= hi:
-            if int(x) == lst[mid-1][0]:
-                print(f"answer is {lst[mid-1][1]}")
-                break
-            elif int(x) > lst[mid-1][0]:
-                lo = mid + 1
-            elif int(x) < lst[mid-1][0]:
-                hi = mid - 1
-            mid = (lo + hi) // 2
-                
-    else:
-        while lo <= hi:
-            if x == lst_by_name[mid-1][1]:
-                print(f"answer is {lst_by_name[mid-1][0]}")
-                break
-            elif x > lst_by_name[mid-1][1]:
-                lo = mid + 1
-            elif x < lst_by_name[mid-1][1]:
-                hi = mid - 1
-            mid = (lo + hi) // 2
-    
                 
     
     
