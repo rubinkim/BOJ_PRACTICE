b847886b631952340915ff937dd1314aa11ddac6
rubinkim <kimyongha2011@gmail.com>
2024-05-22 16:56:52 +0900

    1622.py

--- SIVER_IV/1622_공통순열.py
+++ SIVER_IV/1622_공통순열.py
@@ -10,21 +10,25 @@ street
 (e
 nw
 et)
-"""
+"""   
 while True:
     try:
         a = input()
         b = input()
-        
-        a = list(a)
-        b = list(b)
 
-        a = set(a)
-        b = set(b)
-        
-        common = a.intersection(b)
-        common = sorted(list(common))
-        print("".join(common))
+        a_set = set(a)
+        b_set = set(b)
+
+        x = a_set & b_set
+
+        answer = []
+        for w in x:
+            a_count = a.count(w)
+            b_count = b.count(w)
+            answer.append(w*min(a_count, b_count))
+
+        answer.sort()
+        print(''.join(answer))
     except:
         break
     
