ed37067710e667ae1a1b78b4ae9f4891b1068d69
rubinkim <kimyongha2011@gmail.com>
2024-05-22 16:48:25 +0900

    1622.py

--- SIVER_IV/1622_공통순열.py
+++ SIVER_IV/1622_공통순열.py
@@ -15,8 +15,17 @@ et)
 lst = []
 while True:
     a = input()
+    b = input()
     if a == '':
         break
-    lst.append(a)
     
-print(lst)
+    a = list(a)
+    b = list(b)
+
+    a = set(a)
+    b = set(b)
+    
+    common = a.intersection(b)
+    common = sorted(list(common))
+    print("".join(common))
+    
