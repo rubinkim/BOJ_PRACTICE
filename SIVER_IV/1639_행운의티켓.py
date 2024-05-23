dec1a79ec8a6b7cd01f49d1124a451caf45e62c4
rubinkim <kimyongha2011@gmail.com>
2024-05-23 13:17:10 +0900

    1639_lucky_ticket.py

--- SIVER_IV/1639_행운의티켓.py
+++ SIVER_IV/1639_행운의티켓.py
@@ -42,6 +42,32 @@ if len(s) % 2 == 0:
                 break   
             else:
                 decrement += 2
+                
+else:
+    if sum(s[0:half]) == sum(s[half:len(s)]):
+        print(len(s)-1)
+    elif sum(s[1:half+1]) == sum(s[half+1:len(s)]):
+        print(len(s)-1)
+    else:
+        decrement = 2
+        flag = False
+        while True:
+            if decrement >= len(s):
+                ans = 0
+                print(ans)
+                break
+            half = (len(s) - decrement) // 2
+            ans = 0
+            for i in range(0, decrement+1):
+                if sum(s[i:i+half]) == sum(s[i+half:i+2*half]):
+                    ans = 2 * half
+                    print(ans)
+                    flag = True
+                    break
+            if flag == True:
+                break   
+            else:
+                decrement += 2
 
 
 
