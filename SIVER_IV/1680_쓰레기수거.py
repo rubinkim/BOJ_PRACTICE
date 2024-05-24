2f1e7f4320da430b672a578e9d7b567a64b2b4dc
rubinkim <kimyongha2011@gmail.com>
2024-05-24 17:12:24 +0900

    1680.py

--- SIVER_IV/1680_쓰레기수거.py
+++ SIVER_IV/1680_쓰레기수거.py
@@ -17,52 +17,51 @@
 6
 10)
 """
-t = int(input())
 
-for _ in range(t):
-    dist_lst, weight_lst = [], []
-    total_dist, total_weight = 0, 0
 
-    w, n = map(int, input().split())
-    dist, weight = map(int, input().split())
-    dist_lst.append(dist)
-    weight_lst.append(weight)
-    total_dist += dist
-    total_weight += weight
+dist_lst, weight_lst = [], []
+total_dist, total_weight = 0, 0
 
-    if n == 1:
-        print(2 * dist)
-    else:
-        for _ in range(n-1):
-            dist, weight = map(int, input().split())
-            if total_weight + weight < w:
-                total_weight += weight
-                total_dist += (dist - dist_lst[-1])
+w, n = map(int, input().split())
+dist, weight = map(int, input().split())
+dist_lst.append(dist)
+weight_lst.append(weight)
+total_dist += dist
+total_weight += weight
+
+if n == 1:
+    print(2 * dist)
+else:
+    for _ in range(n-1):
+        dist, weight = map(int, input().split())
+        if total_weight + weight < w:
+            total_weight += weight
+            total_dist += (dist - dist_lst[-1])
+            weight_lst.append(weight)
+            dist_lst.append(dist)
+        elif total_weight + weight == w:
+            if len(dist_lst) < n-1:          
+                total_weight = 0
+                total_dist += (dist - dist_lst[-1] + 2 * dist)
+                weight_lst.append(weight)
+                dist_lst.append(dist)     
+            elif len(dist_lst) == n-1:
+                total_weight = 0
+                total_dist += (dist - dist_lst[-1] + dist)
+                weight_lst.append(weight)
+                dist_lst.append(dist)       
+        elif total_weight + weight > w:
+            if len(dist_lst) < n-1:                
+                total_weight = weight
+                total_dist += (dist - dist_lst[-1] + dist)
                 weight_lst.append(weight)
                 dist_lst.append(dist)
-            elif total_weight + weight == w:
-                if len(dist_lst) < n-1:          
-                    total_weight = 0
-                    total_dist += (dist - dist_lst[-1] + 2 * dist)
-                    weight_lst.append(weight)
-                    dist_lst.append(dist)     
-                elif len(dist_lst) == n-1:
-                    total_weight = 0
-                    total_dist += (dist - dist_lst[-1] + dist)
-                    weight_lst.append(weight)
-                    dist_lst.append(dist)       
-            elif total_weight + weight > w:
-                if len(dist_lst) < n-1:                
-                    total_weight = weight
-                    total_dist += (dist - dist_lst[-1] + dist)
-                    weight_lst.append(weight)
-                    dist_lst.append(dist)
-                elif len(dist_lst) == n-1:
-                    total_weight = 0
-                    total_dist += (dist - dist_lst[-1] + 3 * dist)
-                    weight_lst.append(weight)
-                    dist_lst.append(dist)
+            elif len(dist_lst) == n-1:
+                total_weight = 0
+                total_dist += (dist - dist_lst[-1] + 3 * dist)
+                weight_lst.append(weight)
+                dist_lst.append(dist)
 
-                
-    print(total_dist)
+            
+print(total_dist)
             
