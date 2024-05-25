f3f86a52c94890ff4b5e987b214bb9c3e7e8d43d
rubinkim <kimyongha2011@gmail.com>
2024-05-26 08:22:44 +0900

    more on 1680.py

--- SIVER_IV/1680_쓰레기수거.py
+++ SIVER_IV/1680_쓰레기수거.py
@@ -35,8 +35,55 @@ for _ in range(t):
                 total_weight = 0
                 total_dist = 3 * dist
                 dist_lst.append(dist)
-                weight_lst.append(weight)
-                    
+                weight_lst.append(weight) 
+
+            elif i > 0 and weight == w: 
+                if total_weight + weight < w:
+                    if i == 0:
+                        total_weight += weight
+                        total_dist += dist
+                        dist_lst.append(dist)
+                        weight_lst.append(weight)
+                    elif 0 < i < n-1:
+                        total_weight += weight
+                        total_dist += (dist - dist_lst[-1])
+                        dist_lst.append(dist)
+                        weight_lst.append(weight)
+                    elif i == n-1:
+                        total_weight = 0
+                        total_dist += (dist - dist_lst[-1] + dist)
+                        dist_lst.append(dist)
+                        weight_lst.append(weight)
+                        
+                elif total_weight + weight == w:
+                    if  i == 0:
+                        total_weight = 0
+                        total_dist += 2 * dist
+                        dist_lst.append(dist)
+                        weight_lst.append(weight)                                            
+                    elif 0 < i < n-1:
+                        total_weight = 0
+                        total_dist += (dist - dist_lst[-1] + 2 * dist)
+                        dist_lst.append(dist)
+                        weight_lst.append(weight)
+                    elif i == n-1:
+                        total_weight = 0
+                        total_dist += (dist - dist_lst[-1] + dist)
+                        dist_lst.append(dist)
+                        weight_lst.append(weight)
+                        
+                elif total_weight + weight > w:
+                    if 0 < i < n-1:
+                        total_weight = weight
+                        total_dist += (dist - dist_lst[-1] + 2 * dist)
+                        dist_lst.append(dist)
+                        weight_lst.append(weight)
+                    elif i == n-1:
+                        total_weight = 0
+                        total_dist += (dist - dist_lst[-1] + 3 * dist)
+                        dist_lst.append(dist)
+                        weight_lst.append(weight)                                 
+                  
             elif weight < w: 
                 if total_weight + weight < w:
                     if i == 0:
