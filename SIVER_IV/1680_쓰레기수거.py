0beaabd5d2ce1b3b540007c92d94c96b11e1d8eb
rubinkim <kimyongha2011@gmail.com>
2024-05-25 15:00:10 +0900

    more on 1680.py

--- SIVER_IV/1680_쓰레기수거.py
+++ SIVER_IV/1680_쓰레기수거.py
@@ -55,5 +55,6 @@ for _ in range(t):
                     dist_lst.append(dist)
                     weight_lst.append(weight)
                 elif i == n-1:
-                    
+                    total_weight = 0
+                    total_dist += (dist - dist_lst[-1] + dist)
                                    
