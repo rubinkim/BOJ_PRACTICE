8f580e9a55292953bab48186936b956620f55bbf
rubinkim <kimyongha2011@gmail.com>
2024-05-27 19:28:57 +0900

    1716_polynomial_remains.py

--- SIVER_IV/1716_Polynomial_Remains.py
+++ SIVER_IV/1716_Polynomial_Remains.py
@@ -36,15 +36,14 @@ x = 1
 share_lst, remains_lst = [], []
 
 for i in range(len(coeff_lst)):
-    a_x += coeff_lst[i] * x ** (len(coeff_lst)-1)
+    a_x += coeff_lst[i] * x ** (len(coeff_lst)-1 - i)
     share = a_x // (x ** k + 1)
-    ans = share / x ** (len(coeff_lst)-1-k)
+    ans = share // x ** (len(coeff_lst)-1-k)
     share_lst.append(ans)
     remains = a_x % (x ** k + 1)
     a_x += remains
     
     share_lst.append(share)
-
     
 print(share_lst)
 
